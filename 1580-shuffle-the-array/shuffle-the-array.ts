8cfa3c3de113d3d27fa5e033ed5c2ec599ce4d20
Danish Jajja <135536662+Danishjajja@users.noreply.github.com>
2023-09-25 16:13:28 +0500

    Time: 59 ms (79.15%) | Memory: 45.1 MB (27.41%) - LeetSync

---
+++ 1580-shuffle-the-array/shuffle-the-array.ts
@@ -0,0 +1,12 @@
+var shuffle = function(nums, n) {
+    let result = [];
+    let i=0;
+    let j=n;
+    while(j<2*n){
+        result.push(nums[i]);
+        result.push(nums[j]);
+        i++;
+        j++;
+    }
+    return result;
+};
