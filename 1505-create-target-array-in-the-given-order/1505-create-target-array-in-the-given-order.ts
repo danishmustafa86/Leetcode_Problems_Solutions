df52a5e30ab9858b3c646f09ecbffaf1bf306a41
Danish Jajja <135536662+Danishjajja@users.noreply.github.com>
2023-09-30 23:13:25 +0500

    Time: 64 ms (10.46%), Space: 42.6 MB (89.53%) - LeetHub

--- 1505-create-target-array-in-the-given-order/1505-create-target-array-in-the-given-order.ts
+++ 1505-create-target-array-in-the-given-order/1505-create-target-array-in-the-given-order.ts
@@ -1,7 +1,7 @@
 function createTargetArray(nums: number[], index: number[]): number[] {
-   let target:number[]=[]
+   let targetArray:number[]=[]
    for(let i=0;i<nums.length;i++){
-       target.splice(index[i],0,nums[i])
+       targetArray.splice(index[i],0,nums[i])
    }
-    return target
+    return targetArray
 };
