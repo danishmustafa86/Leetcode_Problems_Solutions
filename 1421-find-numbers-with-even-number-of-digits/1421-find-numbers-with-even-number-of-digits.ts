9d1fe09e1b45bcaadc067df693eac09c25d14e3c
Danish Jajja <135536662+Danishjajja@users.noreply.github.com>
2023-10-08 22:34:59 +0500

    Time: 52 ms (87.17%), Space: 44.8 MB (56.91%) - LeetHub

--- 1421-find-numbers-with-even-number-of-digits/1421-find-numbers-with-even-number-of-digits.ts
+++ 1421-find-numbers-with-even-number-of-digits/1421-find-numbers-with-even-number-of-digits.ts
@@ -1,5 +1,4 @@
-function findNumbers(nums: number[]) {
-  let count = 0;
+function findNumbers(nums: number[],count=0) {
   for (let i = 0; i < nums.length; i++) {
     if (nums[i].toString().length%2==0) {
                 count++
