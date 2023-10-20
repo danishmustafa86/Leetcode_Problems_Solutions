a376a138cc36f6c86e20d8c5b6734cc0d1b6df2f
Danish Jajja <135536662+Danishjajja@users.noreply.github.com>
2023-10-20 08:41:54 +0500

    Time: 57 ms (48.09%), Space: 44 MB (19.28%) - LeetHub

--- 0066-plus-one/0066-plus-one.ts
+++ 0066-plus-one/0066-plus-one.ts
@@ -1,19 +1,20 @@
 function plusOne(digits: number[]): number[] {
-    const n = digits.length;
-    let carry = 1; // Initialize carry to 1 to add 1 to the least significant digit.
-
-    for (let i = n - 1; i >= 0; i--) {
-        const sum = digits[i] + carry;
-        digits[i] = sum % 10; // Update the current digit.
-        carry = Math.floor(sum / 10); // Calculate carry for the next iteration.
+    for (let i = digits.length - 1; i >= 0; i--) {
+        if (digits[i] < 9) {
+            // If the current digit is less than 9, we can increment it and return the array.
+            digits[i] = digits[i] + 1;
+            return digits;
+        }
+        // If the current digit is 9, set it to 0 and continue to the next (left) digit.
+        digits[i] = 0;
+    }
+    
+    // If we reach this point, it means all digits were 9 (e.g., 9, 99, 999, ...).
+    // In this case, we need to add a new most significant digit '1' at the beginning of the array.
+    if (digits[0] === 0) {
+        return [1, ...digits];
     }
 
-    // If there is a carry after the loop, add a new most significant digit.
-    if (carry > 0) {
-        digits.unshift(carry);
-    }
-
+    // If the original number didn't have all 9s, return the updated array.
     return digits;
 }
-
-console.log(plusOne([2, 3, 56, 4, 1, 5, 4, 0, 8, 0, 0, 7, 3, 1, 5, 6, 0, 0]));
