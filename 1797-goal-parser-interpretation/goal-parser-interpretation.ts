323f9f9c85760fdba63dfa28c18e8c5418fb992f
Danish Jajja <135536662+Danishjajja@users.noreply.github.com>
2023-10-28 00:12:18 +0500

    Time: 48 ms (89.23%) | Memory: 43 MB (64.62%) - LeetSync

---
+++ 1797-goal-parser-interpretation/goal-parser-interpretation.ts
@@ -0,0 +1,18 @@
+function interpret(command: string): string {
+    let str:string=""
+for (let i = 0; i < command.length; i++) {
+    if (command[i]=="G") {
+        str=str.concat("G")
+    }
+    else if (command[i]=="(" && command[i+1]==")") {
+        str=str.concat("o")
+    }
+    else if (command[i]=="(" && command[i+3]==")") {
+        str=str.concat("al")
+    }
+    else{
+        str=str.concat("")
+    }
+}
+return str
+};
