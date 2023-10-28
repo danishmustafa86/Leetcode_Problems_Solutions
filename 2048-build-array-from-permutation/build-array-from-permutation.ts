42bb5551de97227e43379832319f399c1b41e430
Danish Jajja <135536662+Danishjajja@users.noreply.github.com>
2023-10-28 22:48:21 +0500

    Time: 75 ms (38.94%) | Memory: 47.2 MB (25.23%) - LeetSync

--- 2048-build-array-from-permutation/build-array-from-permutation.ts
+++ 2048-build-array-from-permutation/build-array-from-permutation.ts
@@ -2,6 +2,6 @@ function buildArray(nums: number[]): number[] {
     let ans:number[] = [];
     for(let index: number = 0; index < nums.length; index++){
       ans.push(nums[nums[index]]);
-    }
+    } 
     return ans;
 };
