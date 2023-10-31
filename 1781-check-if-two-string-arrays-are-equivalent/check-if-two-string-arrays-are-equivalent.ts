a66775e941a51dadb1b2a08e09ee0d81deaaa4e0
Danish Jajja <135536662+Danishjajja@users.noreply.github.com>
2023-10-31 11:56:38 +0500

    Time: 61 ms (25.41%) | Memory: 44.4 MB (29.51%) - LeetSync

--- 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.ts
+++ 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.ts
@@ -1,14 +1,25 @@
+// function arrayStringsAreEqual(word1: string[], word2: string[]): boolean {
+// let n1:string=''
+// let n2:string=''
+// for (let i = 0; i < word1.length; i++) {
+//     n1=n1.concat(word1[i])
+// }
+// for (let j = 0; j < word2.length; j++) {
+//     n2=n2.concat(word2[j])
+// }
+// if (n1===n2) {
+//         return true
+//     }
+// return false
+// };
+
+// OTHEHR METHOD
+
 function arrayStringsAreEqual(word1: string[], word2: string[]): boolean {
-let n1:string=''
-let n2:string=''
-for (let i = 0; i < word1.length; i++) {
-    n1=n1.concat(word1[i])
-}
-for (let j = 0; j < word2.length; j++) {
-    n2=n2.concat(word2[j])
-}
-if (n1===n2) {
+    let n1: string = word1.join("")
+    let n2: string = word2.join("")
+    if (n1 === n2) {
         return true
     }
-return false
+    return false
 };
