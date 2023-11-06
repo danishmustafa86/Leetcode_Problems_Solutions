150d5c3b99a340287d312d4b7037c751ef4ddcbd
Danish Jajja <135536662+Danishjajja@users.noreply.github.com>
2023-11-06 12:03:41 +0500

    Time: 53 ms (70.26%) | Memory: 43.8 MB (13.51%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.ts
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.ts
@@ -1,15 +1,10 @@
-function strStr(haystack = "mississippi", needle = "issip"): number {
-    if (needle === "") {
-        return 0; // Handle the case where the needle is an empty string.
-    }
 
+function strStr(haystack = "mississippi", needle = "issip"): number {
     for (let i = 0; i <= haystack.length - needle.length; i++) {
-        if (haystack.substring(i, i + needle.length) === needle) {
-            return i;
+        if (haystack.substring(i, i+needle.length) == needle) {
+            return i
         }
     }
-
-    return -1; // Needle not found in haystack
+    return -1
 }
-
 console.log(strStr());
