ae6b63f4b7b7982aae63d1f9e2919bc9e31b05aa
Danish Jajja <135536662+Danishjajja@users.noreply.github.com>
2023-11-11 23:01:38 +0500

    Time: 80 ms (16.14%), Space: 52.3 MB (7.85%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.ts
+++ 0125-valid-palindrome/0125-valid-palindrome.ts
@@ -1,15 +1,26 @@
 function isPalindrome(s: string): boolean {
-    let str: string = ""
+    let str: string = "";
     for (let i = 0; i < s.length; i++) {
-        if (s[i] != "," && s[i] != ":" && s[i] != "'" && s[i] != "/" && s[i] != "?" && s[i] != "." && s[i] != "|" && s[i] != " "&& s[i] !="!"&& s[i] !="@"&& s[i] !="#"&& s[i] !="$"&& s[i] !="*"&& s[i] !="&"&& s[i] !="_" && s[i] !="%"&& s[i] !="["&& s[i] !="]"&& s[i] !="{"&& s[i] !="}"&& s[i] !="("&& s[i] !=")"&& s[i] !='"'&& s[i] !='__'&& s[i] !="-"&& s[i] !='+'&& s[i] !='<'&& s[i] !='>'&& s[i] !=';'&& s[i] !='^'&& s[i] !="`"&& s[i] !="~") {
-            str += s[i]
+        if (
+            (s[i] >= "A" && s[i] <= "Z") ||
+            (s[i] >= "a" && s[i] <= "z") ||
+            (s[i] >= "0" && s[i] <= "9")
+        ) {
+            str += s[i];
         }
     }
-    str = str.toLocaleLowerCase()
+    console.log(str);
+    str = str.toLowerCase(); // Corrected line
+
+    console.log(str);
     let reversedStr: string = str.split("").reverse().join("");
+    console.log(reversedStr);
     if (reversedStr === str) {
         return true;
     } else {
         return false;
     };
 }
+
+console.log(isPalindrome("race a car"));
+console.log("jkhuchvjh");
