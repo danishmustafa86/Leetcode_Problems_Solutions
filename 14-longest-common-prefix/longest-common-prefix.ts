1bac98b754554c9b33646a05bed963e8e520fb60
Danish Jajja <135536662+Danishjajja@users.noreply.github.com>
2023-11-13 15:56:55 +0500

    Time: 48 ms (95.17%) | Memory: 44 MB (81.24%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.ts
+++ 14-longest-common-prefix/longest-common-prefix.ts
@@ -1,20 +1,12 @@
 function longestCommonPrefix(strs: string[]): string {
-    let str: string = "";
-    
-    if (strs.length === 0) {
-        return str;
-    }
-
-    for (let j = 0; j < strs[0].length; j++) {
-        for (let i = 1; i < strs.length; i++) {
-            if ( strs[i][j] !== strs[0][j]) {
-                return str;
-            }
+  let str:string=""
+  for (let j = 0; j < strs[0].length; j++) {
+    for (let i = 0; i < strs.length; i++) {
+        if (strs[0][j]!=strs[i][j]) {
+            return str
         }
-        str += strs[0][j];
-    }
-
-    return str;
-}
-
-console.log(longestCommonPrefix(["flower", "flow", "flight"]));
+    }    
+    str+=strs[0][j]
+  }
+  return str
+};
