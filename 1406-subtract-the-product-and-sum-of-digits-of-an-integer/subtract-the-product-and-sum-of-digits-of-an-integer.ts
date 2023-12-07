00c1734f5304caa7f3859c3778abdbf5537786cf
Danish Jajja <135536662+Danishjajja@users.noreply.github.com>
2023-12-07 22:17:34 +0500

    Time: 61 ms (13.91%) | Memory: 42.9 MB (46.96%) - LeetSync

--- 1406-subtract-the-product-and-sum-of-digits-of-an-integer/subtract-the-product-and-sum-of-digits-of-an-integer.ts
+++ 1406-subtract-the-product-and-sum-of-digits-of-an-integer/subtract-the-product-and-sum-of-digits-of-an-integer.ts
@@ -1,12 +1,12 @@
-function subtractProductAndSum(n: number) {
-let st:string=n.toString()
-let product:number=1;
-let sum:number=0
-    for(let i=0;i<st.length;i++){
-let n2:number=parseInt(st[i])
-product=product*n2
-sum=sum+n2
-}
-return product-sum
-};
 
+function subtractProductAndSum(n: number) {
+    let st: string = n.toString()
+    let product: number = 1;
+    let sum: number = 0
+    for (let i = 0; i < st.length; i++) {
+        let n2: number = parseInt(st[i])
+        product = product * n2
+        sum = sum + n2
+    }
+    return product - sum
+};
