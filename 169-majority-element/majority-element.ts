66f9dc8911d612e4d544654129da2472fb1502c7
Danish Jajja <135536662+Danishjajja@users.noreply.github.com>
2023-12-10 18:25:33 +0500

    Time: 61 ms (64.77%) | Memory: 45.4 MB (88.26%) - LeetSync

--- 169-majority-element/majority-element.ts
+++ 169-majority-element/majority-element.ts
@@ -1,3 +1,16 @@
 function majorityElement(nums: number[]): number {
-    return nums.sort()[Math.floor(nums.length / 2)];
+    let n : number = nums.length , carry : number = 1 , element: number = nums[0]
+
+    for(let i =1;i<n;i++){
+        if(element === nums[i]){
+            carry++
+        }else{
+            carry--
+        }
+        if(carry < 0){
+            carry++
+            element = nums[i]
+        }
+    }
+    return element
 };
