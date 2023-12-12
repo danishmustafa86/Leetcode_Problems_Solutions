16dac607596169382579df6456d0282878c84216
Danish Jajja <135536662+Danishjajja@users.noreply.github.com>
2023-12-12 22:22:10 +0500

    Time: 83 ms (15.72%) | Memory: 45.8 MB (31.17%) - LeetSync

--- 268-missing-number/missing-number.ts
+++ 268-missing-number/missing-number.ts
@@ -1,21 +1,75 @@
+// function missingNumber(nums: number[]): number {
+//     nums=nums.sort()
+//     let n=nums.length
+//     let n1:number=0
+//     for(let i=0;i<n;i++){
+//         if(nums[i]!=(nums[i+1]-1)){
+//             n1=nums[i]-1
+//         }
+//     }
+//     return n1
+// };
+
+
+
+
+
 function missingNumber(nums: number[]): number {
-    nums=nums.sort()
-    let n=nums.length
-    let n1:number=0
-    for(let i=0;i<=n;i++){
-        if(!nums.includes(i)){
-           return i
+    nums = nums.sort((a, b) => a - b); // Sorting in ascending order
+
+    let n = nums.length;
+    let n1: number = 0;
+ if (nums[0] !== 0) {
+        return 0;
+    }
+
+    // Check if n is missing at the end
+    if (nums[n - 1] !== n) {
+        return n;
+    }
+    for (let i = 0; i < n; i++) {
+        if (nums[i] !== i) {
+            n1 = i;
+            break;
         }
     }
-};
+
+    // If no missing number is found at the end
+    if (n1 === 0) {
+        n1 = n;
+    }
+
+    return n1;
+}
+
+
+
+
+// function missingNumber(nums: number[]): number {
+//     nums = nums.sort();
+//     let n = nums.length;
+//     let n1: number = 0;
+
+//     for (let i = 0; i < n; i++) {
+//         if (nums[i] !== i) {
+//             n1 =i;
+//         }
+//     }
+//     if(n1===0){
+//     n1=n
+//         }
+//     return n1;
+// }
+
+
+
 // function missingNumber(nums: number[]): number {
 //     nums=nums.sort()
 //     let n=nums.length
 //     let n1:number=0
 //     for(let i=0;i<=n;i++){
-//         if(nums[i]!=(nums[i+1]-1)){
-//             n1=nums[i]+1
+//         if(!nums.includes(i)){
+//            return i
 //         }
 //     }
-//     return n1
 // };
