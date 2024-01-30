4a088ab2f7aa3783a9ee9caca4022b47c3fa4c53
Danish Jajja <135536662+Danishjajja@users.noreply.github.com>
2024-01-30 22:36:15 +0500

    Time: 67 ms (37.50%) | Memory: 52 MB (12.50%) - LeetSync

--- 1910-check-if-binary-string-has-at-most-one-segment-of-ones/check-if-binary-string-has-at-most-one-segment-of-ones.ts
+++ 1910-check-if-binary-string-has-at-most-one-segment-of-ones/check-if-binary-string-has-at-most-one-segment-of-ones.ts
@@ -1,10 +1,9 @@
 function checkOnesSegment(s: string): boolean {
-   
-    for (let i = 0; i < s.length; i++) {
-        if (s[i]=="0"&&s[i+1]=="1") {
-        return false
-        }  
-       
-}
-return true      
+    // let bool:boolean=true
+    for(let i=0;i<s.length;i++ ){
+        if(s[i]=="0" && s[i+1]==="1"){
+            return false
+        }
+    }
+    return true
 };
