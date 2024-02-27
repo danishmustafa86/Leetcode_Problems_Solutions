18a96c840a192d6e45d195ad24ecc5266347e017
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-02-28 00:11:35 +0500

    Time: 96 ms (70.11%) | Memory: 57.3 MB (14.32%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.ts
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.ts
@@ -1,6 +1,5 @@
 function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
-    let num=nums1.concat(nums2)
-    num=num.sort((a,b) => a-b)
+    let num=nums1.concat(nums2).sort((a,b) => a-b)
     if( num.length%2==1){
         return num[Math.floor(num.length/2)]
     } if(num.length%2==0){
