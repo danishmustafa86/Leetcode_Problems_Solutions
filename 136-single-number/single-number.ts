33c5c4a30668ed1b1092f63f996812e9e9711619
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-03-25 23:24:18 +0500

    Time: 76 ms (29.49%) | Memory: 53 MB (50.18%) - LeetSync

--- 136-single-number/single-number.ts
+++ 136-single-number/single-number.ts
@@ -1,10 +1,22 @@
 function singleNumber(nums: number[]): number {
-    let result = 0;
-    for (let i = 0; i < nums.length; i++) {
-        result ^= nums[i];
-    }
-    return result;
+nums.sort((a,b)=> a-b)
+for (let i=0;i<nums.length;i+=2){
+    if(nums[i]!=nums[i+1]){
+        return nums[i]
+    }}
+    return -1
 }
+
+
+
+
+// function singleNumber(nums: number[]): number {
+//     let result = 0;
+//     for (let i = 0; i < nums.length; i++) {
+//         result ^= nums[i];
+//     }
+//     return result;
+// }
 
 
 // function singleNumber(nums: number[]): number {
