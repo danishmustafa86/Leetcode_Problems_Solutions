50cb3276efb18576bf6cd902eaac3ce9eb78757b
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-04-05 22:48:37 +0500

    Time: 1137 ms (8.74%) | Memory: 28 MB (36.10%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -5,8 +5,14 @@ class Solution(object):
         :type k: int
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        k = k % len(nums)
-        nums[:] = nums[-k:] + nums[:-k]  # Modify nums in-place
+        # k = k% len(nums)
+        # nums[:] = nums[-k:] + nums[:-k]  # Modify nums in-place
+        k = k% len(nums)
+        while k > 0:
+            deleted = nums[-1]
+            del nums[-1]
+            nums.insert(0, deleted)
+            k -= 1
 
 # Better if my upper answer not works
 # 2nd one works I tested
