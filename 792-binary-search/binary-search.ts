ad291763ac5f5c70126cd5153c70db97de0ce40b
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-04-25 10:29:37 +0500

    Time: 44 ms (98.86%) | Memory: 54.3 MB (7.82%) - LeetSync

---
+++ 792-binary-search/binary-search.ts
@@ -0,0 +1,8 @@
+function search(nums: number[], target: number): number {
+    for (let i=0; i < nums.length; i++){
+        if(target==nums[i]){
+            return i
+        }
+    }
+    return -1
+};
