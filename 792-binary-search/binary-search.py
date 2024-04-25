3ce861c3166559210b116387bb20ae7225fcf587
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-04-25 10:42:04 +0500

    Time: 200 ms (17.13%) | Memory: 18.1 MB (50.13%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -1,7 +1,6 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        index = -1
         for i in range(len(nums)):
             if target == nums[i]:
-                index = i
-        return index
+                return i
+        return -1
