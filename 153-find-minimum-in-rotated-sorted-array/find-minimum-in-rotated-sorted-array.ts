665cb09d303d235c978b014a159c7031e20af4e4
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-04-25 10:52:33 +0500

    Time: 59 ms (45.89%) | Memory: 51.5 MB (27.44%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.ts
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.ts
@@ -1,4 +1,30 @@
 function findMin(nums: number[]): number {
-    nums = nums.sort((a,b) => a-b)
-    return nums[0]
-};
+    let min = nums[0]
+    for (let i=1; i<nums.length; i++){
+        if (min > nums[i]){
+            min = nums[i]
+        }
+    }
+    return min
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // nums = nums.sort((a,b) => a-b)
+    // return nums[0]
