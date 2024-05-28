88958e6dd6dc06bc434c0a53863ad118b1602834
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-05-29 00:19:30 +0500

    Time: 49 ms (92.64%) | Memory: 51.3 MB (59.86%) - LeetSync

--- 27-remove-element/remove-element.ts
+++ 27-remove-element/remove-element.ts
@@ -1,10 +1,10 @@
 function removeElement(nums: number[], val: number): number {
-    let slow=0
-    for(let i=0;i<nums.length;i++){
+    let slow = 0, i = 0;
+    while (i<nums.length) {
         if(nums[i] !=val){
             nums[slow]=nums[i]
             slow+=1
-        }
+        }i++
     }
     // let k=nums.length
     return slow
