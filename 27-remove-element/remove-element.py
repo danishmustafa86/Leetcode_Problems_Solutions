8f50f0038f00759360f414db2c330878d41e05fd
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-05-29 00:11:30 +0500

    Time: 27 ms (97.66%) | Memory: 16.4 MB (91.55%) - LeetSync

---
+++ 27-remove-element/remove-element.py
@@ -0,0 +1,9 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        slow=fast=0
+        while fast<len(nums):
+            if nums[fast] != val:
+                nums[slow]=nums[fast]
+                slow+=1
+            fast+=1
+        return slow
