be021dd6d424fd17ed6318267369e6ca20cfac04
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-06-08 00:37:51 +0500

    Time: 59 ms (42.96%) | Memory: 16.6 MB (90.20%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,31 +1,27 @@
-# class Solution:
-#     def lengthOfLongestSubstring(self, s: str) -> int:
-#         i, j, max_len=0, 0, 0
-#         my_set = set()
-#         while j < len(s):
-#             if s[j] not in my_set:
-#                 my_set.add(s[j])
-#                 j += 1
-#                 max_len = max(max_len, len(my_set))
-#                 print("my_set is", my_set)
-#             else:
-#                 my_set.remove(s[i])
-#                 print("my_set is", my_set)
-#                 i += 1
-#         return max_len
-
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        i, j, max_len = 0, 0, 0
-        my_set = set()
-        while j < len(s):
-            if s[j] not in my_set:
-                my_set.add(s[j])
-                j += 1
-                max_len = max(max_len, len(my_set))
-                # print("my_set is", my_set)
-            else:
-                my_set.remove(s[i])
-                i += 1
+        left=0
+        sett=set()
+        max_len=0
+        for i in range(len(s)):
+            while s[i] in sett:
+                sett.remove(s[left])
+                left+=1
+            sett.add(s[i])
+            max_len=max(max_len,i-left+1)
         return max_len
 
+# class Solution:
+#     def lengthOfLongestSubstring(self, s: str) -> int:
+#         left = 0
+#         sett = set()
+#         max_len = 0
+        
+#         for right in range(len(s)):
+#             while s[right] in sett:
+#                 sett.remove(s[left])
+#                 left += 1
+#             sett.add(s[right])
+#             max_len = max(max_len, right - left + 1)
+        
+#         return max_len
