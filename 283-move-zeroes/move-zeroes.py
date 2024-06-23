a2c2fc5e573632eeda44b9fb396f28b8ecbef8b2
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-06-23 12:21:51 +0500

    Time: 114 ms (96.67%) | Memory: 18.1 MB (21.08%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -1,28 +1,5 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        # lf,rt=0,len(nums)
-        # while lf<rt:
-        #     if nums[lf]==0:
-        #         del(nums[lf])
-        #         nums.append(0)
-        #         lf-=1
-        #     lf+=1
-        #     rt-=1
-
-        # i=0
-        # while i < len(nums):
-        #     if nums[i]==0:
-        #         del nums[i]
-        #         nums.append(0)
-        #     else: 
-        #         i+=1
-
-
-        # for i in range(len(nums)-1):
-        #     if nums[i]==0:
-        #         del(nums[i])
-        #         nums.append(0)
-        #         i-=1
 
         first_index=0
         for i in range(len(nums)):
@@ -34,9 +11,3 @@ class Solution:
 
 
 
-
-
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        
