93b21b1dac0f09d3e5c9467834a98a877ac6b8d7
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-07-02 16:59:34 +0500

    Time: 39 ms (52.33%) | Memory: 16.5 MB (93.58%) - LeetSync

--- 61-rotate-list/rotate-list.py
+++ 61-rotate-list/rotate-list.py
@@ -1,31 +1,25 @@
+# Definition for singly-linked list.
 # class ListNode:
-    # def __init__(self, val=0, next=None):
-    #     self.val = val
-    #     self.next = next
-
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def rotateRight(self, head: ListNode, k: int) -> ListNode:
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
         if not head or not head.next or k == 0:
             return head
-        
-        # Step 1: Determine the length of the list
-        length = 1
+
+        lenght = 1
         old_tail = head
         while old_tail.next:
             old_tail = old_tail.next
-            length += 1
-        
-        # Step 2: Make the list circular
+            lenght += 1
+
         old_tail.next = head
-        
-        # Step 3: Find the new tail and new head
-        k = k % length
+
+        k = k%lenght
         new_tail = head
-        for _ in range(length - k - 1):  # Here is the loop with `_`
+        for _ in range(lenght - k - 1):
             new_tail = new_tail.next
         new_head = new_tail.next
-        
-        # Step 4: Break the circular list
         new_tail.next = None
-        
         return new_head
