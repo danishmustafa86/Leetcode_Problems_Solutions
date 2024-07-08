37364570ce6e13c7e46e8a774217c836f21ad8e6
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-07-08 10:01:21 +0500

    Time: 31 ms (87.21%) | Memory: 16.5 MB (24.62%) - LeetSync

--- 231-power-of-two/power-of-two.py
+++ 231-power-of-two/power-of-two.py
@@ -3,13 +3,12 @@ class Solution:
         if n == 1:
             return True
         if n/2 > n//2:
-            print("hellossofaghso")
             return False
         num = n
         twos = 1
         while num//2 >= 2:
-            # if num/2 > num//2:
-            #     return False
+            if num/2 > num//2:
+                return False
             num = num//2
             twos += 1 
         if 2**twos == n:
