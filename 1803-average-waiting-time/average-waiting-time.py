4faeb2a497659a645bc9963e57f78a00c951e9f6
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-07-09 09:50:41 +0500

    Time: 748 ms (59.45%) | Memory: 57.3 MB (9.97%) - LeetSync

--- 1803-average-waiting-time/average-waiting-time.py
+++ 1803-average-waiting-time/average-waiting-time.py
@@ -1,17 +1,11 @@
 class Solution:
     def averageWaitingTime(self, customers: List[List[int]]) -> float:
-        arr = []
-        time,wait = customers[0][0], customers[0][0]
-        for i in range(len(customers)):
-            time += customers[i][1]
-            print(time)
-            wait = time - customers[i][0]
-            arr.append(wait)
+        cur_time = 0
+        total_time = 0
+        for arrival, duration in customers:
+            if cur_time < arrival:
+                cur_time = arrival
+            cur_time += duration
+            total_time += cur_time - arrival
 
-            if customers[i][0]  > time - customers[i][1]:
-                time = customers[i][0] + customers[i][1]
-                wait = time - customers[i][0]
-                arr.pop()
-                arr.append(wait)
-        print(arr,sum(arr))
-        return sum(arr) / len(customers)
+        return total_time / len(customers)
