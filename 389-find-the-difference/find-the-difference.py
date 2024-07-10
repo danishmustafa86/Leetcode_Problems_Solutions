8f93f484814c38d34e1a27c44effb31a327bc538
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-07-10 09:26:51 +0500

    Time: 48 ms (11.03%) | Memory: 16.6 MB (18.26%) - LeetSync

--- 389-find-the-difference/find-the-difference.py
+++ 389-find-the-difference/find-the-difference.py
@@ -2,6 +2,5 @@ class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
         t_list = list(t)
         for val in s:
-            if val in t_list:
-                t_list.remove(val)
+            t_list.remove(val)
         return "".join(t_list)
