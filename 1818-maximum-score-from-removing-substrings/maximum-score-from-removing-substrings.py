1f4527f52b4de1315b473db7f695529b15fba850
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-07-12 13:18:12 +0500

    Time: 220 ms (78.57%) | Memory: 18.3 MB (38.96%) - LeetSync

--- 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.py
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.py
@@ -1,21 +1,19 @@
 class Solution:
     def maximumGain(self, s: str, x: int, y: int) -> int:
-        def remove_and_score(s: str, first: str, second: str, score: int) -> (str, int):
+        def r_s(s:str,first:str,second:str,score:int) -> (str,int):
             stack = []
-            current_score = 0
+            cur_score = 0
             for char in s:
                 if stack and stack[-1] == first and char == second:
                     stack.pop()
-                    current_score += score
+                    cur_score += score
                 else:
                     stack.append(char)
-            return ''.join(stack), current_score
-
-        if x > y:
-            s, score1 = remove_and_score(s, 'a', 'b', x)
-            s, score2 = remove_and_score(s, 'b', 'a', y)
+            return "".join(stack), cur_score
+        if x>y:
+            s, score1 = r_s(s, "a", "b", x)
+            s, score2 = r_s(s, "b", "a", y)
         else:
-            s, score1 = remove_and_score(s, 'b', 'a', y)
-            s, score2 = remove_and_score(s, 'a', 'b', x)
-        
+            s, score2 = r_s(s, "b", "a", y)
+            s, score1 = r_s(s, "a", "b", x)
         return score1 + score2
