f6df8c32d2b86ad140688cb4ac8fa6b884fb18d8
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-07-22 08:51:27 +0500

    Time: 99 ms (82.68%) | Memory: 17.1 MB (14.55%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -4,10 +4,11 @@ class Solution:
         sorted_heights = [(val,idx) for idx,val in enumerate(heights)]
         sorted_heights.sort()
         sorted_heights.reverse()
+
         new_names = [None] * len(names)
         indices = [tup[1]   for tup in sorted_heights]
-        print(indices)
+
         for i, val in enumerate(indices):
             new_names[i] = names[val]
-        print(new_names)
+            
         return new_names
