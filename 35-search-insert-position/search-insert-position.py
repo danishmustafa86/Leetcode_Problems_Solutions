158e7954217a7f0d6b035c44831b1a5e2f830ad3
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-07-26 16:22:23 +0500

    Time: 42 ms (93.00%) | Memory: 17.2 MB (51.19%) - LeetSync

--- 35-search-insert-position/search-insert-position.py
+++ 35-search-insert-position/search-insert-position.py
@@ -1,21 +1,51 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        if target < nums[0] :
-            return 0
-        if target > nums[-1]:
-            return len(nums)
-        # if target > nums[-1]:
-        #     return len(nums)
-        l,r = 0, len(nums)-1
-        while l<=r:
-            if target == nums[l]:
-                return l 
-            elif target == nums[r]:
-                return r
-            elif target < nums[l] and target > nums[l-1]:
-                return l
-            elif target < nums[r] and target > nums[r-1]:
-                return r
-            l += 1
-            r -= 1
-        return len(nums)
+        l, r = 0, len(nums)
+        while l < r:
+            mid = (l+r) // 2
+            if (target > nums[mid]):
+                l = mid + 1
+            else:
+                r = mid
+        return l
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # if  target < nums[0]:
+        #     return 0
+        # l, r = 0 , len(nums)-1
+        # while l < r:
+        #     mid = l+r//2
+        #     if target < nums[mid]:
+        #         r = mid -1 
+        #     elif target > nums[mid]:
+        #         l = mid 
+        #     elif target == nums[mid]:
+        #         return mid
+        #     elif target > nums[mid] and target < nums[mid+1]:
+        #         return mid 
+        #     elif target > nums[mid] and target < nums[mid+1]:
+        #         return mid 
+        #     elif target > nums[mid] and target < nums[mid+1]:
+        #         return mid 
+        # return len(nums)
+                
+
+            
+            
