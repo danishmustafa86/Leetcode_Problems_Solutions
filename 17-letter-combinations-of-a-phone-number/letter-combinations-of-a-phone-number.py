1c7abbbc690065beb10d8b21e39225ca0a5782b1
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-07-28 09:29:10 +0500

    Time: 34 ms (67.04%) | Memory: 16.7 MB (7.87%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,25 +1,45 @@
+# class Solution:
+#     def letterCombinations(self, digits: str) -> List[str]:
+#         hsh = {2:"abc",3:"def",4:"ghi",5:"jkl",6:"mno",7:"pqrs",8:"tuv",9:"wxyz"}
+#         n = 0
+#         l = len(digits)
+#         if not digits :
+#             return []
+#         if l == 1 :
+#             n = int(digits)
+#             return list(hsh[n])
+#         old = [""] 
+#         for i in digits:
+#             new = []
+
+#             # n = int(digits[i])
+#             # cur = list(hsh[n])
+#             for j in old:
+#                 for k in hsh[int(i)]:
+#                     new.append(j + k)
+#             old = new
+                
+#         return new
+
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         hsh = {2:"abc",3:"def",4:"ghi",5:"jkl",6:"mno",7:"pqrs",8:"tuv",9:"wxyz"}
         n = 0
         l = len(digits)
-        if l == 0 :
+        if not digits :
             return []
         if l == 1 :
             n = int(digits)
             return list(hsh[n])
-        old = [""] 
-        for i in digits:
+        old =  [""]
+        for i in range(l - 1,-1,-1):
             new = []
 
-            # n = int(digits[i])
-            # cur = list(hsh[n])
-            for j in old:
-                for k in hsh[int(i)]:
+            n = int(digits[i])
+            cur = list(hsh[n])
+            for j in cur:
+                for k in old:
                     new.append(j + k)
             old = new
                 
-
-
-
         return new
