54d5a6d0d9061f9225c24928a95aef0097a0bf75
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-08-01 15:34:35 +0500

    Time: 51 ms (50.07%) | Memory: 16.6 MB (57.64%) - LeetSync

--- 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.py
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.py
@@ -2,16 +2,49 @@ class Solution:
     def frequencySort(self, nums: List[int]) -> List[int]:
         nums.sort()
         nums.reverse()
-        arr = []
-        x = {}
+        hsh = {}
         for val in nums:
-            if val in x:
-                x[val] += 1
+            if val in hsh:
+                hsh[val] += 1
             else:
-                x[val] = 1
-        new_dict = {k: v for k, v in sorted(x.items(), key=lambda item: item[1])}
-        for val in new_dict:
-            while new_dict[val] > 0:
-                arr.append(val)
-                new_dict[val] -= 1
-        return arr
+                hsh[val] = 1
+        sorted_hsh = {k:v for k,v in sorted(hsh.items() , key = lambda item:item[1])}
+        ans = []
+        for val in sorted_hsh:
+            while hsh[val] > 0:
+                ans.append(val)
+                hsh[val] -= 1
+        return ans
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # nums.sort()
+        # nums.reverse()
+        # x = {}
+        # for val in nums:
+        #     if val in x:
+        #         x[val] += 1
+        #     else:
+        #         x[val] = 1
+        # new_dict = {k: v for k, v in sorted(x.items(), key=lambda item: item[1])}
+        # arr = []
+        # for val in new_dict:
+        #     while new_dict[val] > 0:
+        #         arr.append(val)
+        #         new_dict[val] -= 1
+        # return arr
