04256dd3d283e64fa7d4849f217e6f04563ec4ea
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-08-15 15:19:01 +0500

    Time: 33 ms (79.44%) | Memory: 16.4 MB (92.62%) - LeetSync

--- 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.py
+++ 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.py
@@ -1,22 +1,17 @@
 class Solution:
     def countGoodSubstrings(self, s: str) -> int:
         count = 0
-        if len(s) < 3:
+        if len(s)<3:
             return 0
-        
-        stack = list(s[:3])
-        
-        # Check the first window
-        if len(set(stack)) == 3:
+        swin = list(s[:3])
+
+        if len(set(swin)) == 3:
             count += 1
-        
         for i in range(3, len(s)):
-            # Slide the window
-            stack.pop(0)
-            stack.append(s[i])
-            
-            # Check the current window
-            if len(set(stack)) == 3:
+            swin.pop(0)
+            swin.append(s[i])
+            if len(set(swin)) == 3:
                 count += 1
-                
-        return count
+        return count 
+
+
