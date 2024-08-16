4dd2235bfffb0acec142c063bf9855524bd48f5a
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-08-16 12:50:35 +0500

    Time: 83 ms (71.56%) | Memory: 16.6 MB (70.73%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -3,11 +3,11 @@ class Solution:
         count = {}
         res = 0
         left = 0
-        # maxf = 0
+        maxf = 0
         for right in range(len(s)):
             count[s[right]] = 1 + count.get(s[right],0)
-            # maxf = max(maxf, count[s[right]])
-            while (right - left + 1) - max(count.values()) > k:
+            maxf = max(maxf, count[s[right]])
+            while (right - left + 1) - maxf > k:
                 count[s[left]] -= 1
                 left += 1
             res = max(res,right - left + 1) 
