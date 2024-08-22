51c3db16f830da42407877a7e0135b478e74e2ad
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-08-22 16:59:03 +0500

    Time: 31 ms (87.90%) | Memory: 16.8 MB (39.61%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.py
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.py
@@ -6,29 +6,23 @@
 #         self.right = right
 class Solution:
     def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        maxHeight=0
-        def dfs (cur: Optional[TreeNode], index : int):
-            nonlocal maxHeight
-            if not cur:
-                maxHeight= max( maxHeight, index)
-                return
-            dfs (cur.left, index+1) 
-            dfs (cur.right, index+1) 
-        dfs(root,0)
-        # step_2
-        ansList = [[] for _ in range(maxHeight)]
-        # step_3
-        def dfsFunc (cur: Optional[TreeNode], index : int):
-            nonlocal maxHeight
-            if not cur:
-                # maxHeight= max( maxHeight, index)
-                return
-            ansList[index].append(cur.val)
-            dfsFunc (cur.left, index+1) 
-            dfsFunc (cur.right, index+1) 
-        dfsFunc(root,0)    
-        # step_4
-        for i in range(maxHeight):
-            if i%2 == 1:
-                ansList[i] = ansList[i][::-1]
-        return ansList
+        if not root:
+            return []
+        q = deque([root])
+        zigzag = []
+        level = 0
+        while q:
+            cur = []
+            level += 1
+            for i in range(len(q)):
+                node = q.popleft()
+                cur.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            if level%2 == 0:
+                zigzag.append(cur[::-1])
+            else:
+                zigzag.append(cur)
+        return zigzag
