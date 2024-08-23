4ce37a024f7903dbfa48979466600db320701571
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-08-23 15:43:40 +0500

    Time: 995 ms (83.20%) | Memory: 20.2 MB (76.91%) - LeetSync

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.py
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.py
@@ -4,12 +4,29 @@ class Solution:
         for i in range(len(nums)-2):
             if nums[i] == 0:
                 flips += 1
-                for j in range(i,i+3):
-                    if nums[j] == 0:
-                        nums[j] = 1
-                    else:
-                        nums[j] = 0
-        return flips if nums.count(1) == len(nums) else -1
+                nums[i] = 1
+                nums[i+1] ^= 1
+                nums[i+2] ^= 1
+        return flips if nums[-1] == 1 and nums[-2] == 1 else -1
+
+
+
+
+
+
+
+# class Solution:
+#     def minOperations(self, nums: List[int]) -> int:
+#         flips = 0
+#         for i in range(len(nums)-2):
+#             if nums[i] == 0:
+#                 flips += 1
+#                 for j in range(i,i+3):
+#                     if nums[j] == 0:
+#                         nums[j] = 1
+#                     else:
+#                         nums[j] = 0
+#         return flips if nums.count(1) == len(nums) else -1
 
 
 
