89cab28cf574d1bf8fefc1ac36563435a44b28b4
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-08-23 10:05:58 +0500

    Time: 1014 ms (64.42%) | Memory: 21 MB (7.61%) - LeetSync

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.py
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.py
@@ -1,12 +1,29 @@
+# class Solution:
+#     def minOperations(self, nums: List[int]) -> int:
+#         flip = 0
+#         for i in range(len(nums)-2):
+#             if nums[i] == 0:
+#                 flip += 1
+#                 for j in range(i,i+3):
+#                     if nums[j] == 0:
+#                         nums[j] = 1
+#                     else:
+#                         nums[j] = 0
+#         return flip if nums.count(1) == len(nums) else -1
+
+
+
+
+
+
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
         flip = 0
-        for i in range(len(nums)-2):
+        for i in range(len(nums) - 2):
             if nums[i] == 0:
                 flip += 1
-                for j in range(i,i+3):
-                    if nums[j] == 0:
-                        nums[j] = 1
-                    else:
-                        nums[j] = 0
-        return flip if nums.count(1) == len(nums) else -1
+                nums[i] == 1
+                nums[i+1] ^= 1
+                nums[i+2] ^= 1
+        
+        return flip if nums[-1] == 1 and nums[-2] == 1 else -1
