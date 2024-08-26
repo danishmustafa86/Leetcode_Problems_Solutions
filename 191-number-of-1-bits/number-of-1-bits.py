4de08626640a3d55f21c806cd52b48965e36136b
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-08-26 12:23:01 +0500

    Time: 43 ms (10.34%) | Memory: 16.4 MB (67.28%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.py
+++ 191-number-of-1-bits/number-of-1-bits.py
@@ -2,6 +2,23 @@ class Solution:
     def hammingWeight(self, n: int) -> int:
         res = 0
         while n:
-            res += n % 2
-            n = n //2 
+            n = n & (n-1)
+            res += 1
         return res
+
+
+
+
+
+
+
+
+
+
+# class Solution:
+#     def hammingWeight(self, n: int) -> int:
+#         res = 0
+#         while n:
+#             res += n % 2
+#             n = n // 2 
+#         return res
