111e8702c1e2c6107a4312f0d1d62c108a70926d
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-08-26 13:03:55 +0500

    Time: 126 ms (73.52%) | Memory: 31.1 MB (74.89%) - LeetSync

--- 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
@@ -7,11 +7,36 @@
 
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
-        def solve(root,val):
-            if root:
-                k = solve(root.left, max(val,root.val)) + solve(root.right, max(val,root.val))
-                if root.val >= val:
-                    k+=1
-                return k
-            return 0
-        return solve(root,root.val)
+        def dfs(node,maxval):
+            if not node:
+                return 0
+            res = 1 if node.val >= maxval else 0
+            maxval = max(maxval, node.val)
+            res += dfs(node.left, maxval)
+            res += dfs(node.right, maxval)
+            return res
+        return dfs(root, root.val)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Solution:
+#     def goodNodes(self, root: TreeNode) -> int:
+#         def solve(root,val):
+#             if root:
+#                 k = solve(root.left, max(val,root.val)) + solve(root.right, max(val,root.val))
+#                 if root.val >= val:
+#                     k+=1
+#                 return k
+#             return 0
+#         return solve(root,root.val)
