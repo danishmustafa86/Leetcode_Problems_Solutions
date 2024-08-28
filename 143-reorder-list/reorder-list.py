27c26ef36f93f6a007c0aa5c922359de006b4336
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-08-28 13:26:55 +0500

    Time: 61 ms (27.06%) | Memory: 24.6 MB (85.67%) - LeetSync

--- 143-reorder-list/reorder-list.py
+++ 143-reorder-list/reorder-list.py
@@ -5,26 +5,71 @@
 #         self.next = next
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
-        """
-        Do not return anything, modify head in-place instead.
-        """
-        s,f=head,head.next
+        s,f = head, head.next
         while f and f.next:
-            s=s.next
-            f=f.next.next
-        prev=None
-        curr=s.next
-        while curr:
-            new= curr.next
-            curr.next=prev
-            prev=curr
-            curr=new
-        s.next=None
-        l1=head
-        l2=prev
-        while l2 != None:
-            t1=l1.next
-            t2=l2.next
-            l1.next=l2
-            l2.next=t1
-            l1,l2=t1,t2
+            s = s.next
+            f = f.next.next
+        prev = None
+        cur = s.next
+        while cur:
+            nxt = cur.next
+            cur.next = prev
+            prev = cur
+            cur = nxt
+        s.next = None
+        l1 = head
+        l2 = prev
+        while l2 :
+            t1 = l1.next
+            t2 = l2.next
+            l1.next = l2
+            l2.next = t1
+            l1,l2 = t1,t2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # s,f=head,head.next
+        # while f and f.next:
+        #     s=s.next
+        #     f=f.next.next
+        # prev=None
+        # curr=s.next
+        # while curr:
+        #     new= curr.next
+        #     curr.next=prev
+        #     prev=curr
+        #     curr=new
+        # s.next=None
+        # l1=head
+        # l2=prev
+        # while l2 != None:
+        #     t1=l1.next
+        #     t2=l2.next
+        #     l1.next=l2
+        #     l2.next=t1
+        #     l1,l2=t1,t2
