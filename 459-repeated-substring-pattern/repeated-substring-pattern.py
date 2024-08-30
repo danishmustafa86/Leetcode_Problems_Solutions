e630d7e96c0d36b304e5135d85c6d29b4f6596c0
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-08-30 17:23:13 +0500

    Time: 127 ms (18.17%) | Memory: 16.6 MB (49.60%) - LeetSync

--- 459-repeated-substring-pattern/repeated-substring-pattern.py
+++ 459-repeated-substring-pattern/repeated-substring-pattern.py
@@ -1,28 +1,28 @@
-class Solution:
-    def repeatedSubstringPattern(self, s: str) -> bool:
-        
-        newstr = s * 2
-        print(newstr)
-        newstr = newstr[1:-1]
-        print(newstr)
-        if (s in newstr):
-            return True
-        else:
-            return False
-        
 # class Solution:
 #     def repeatedSubstringPattern(self, s: str) -> bool:
+        
+#         newstr = s * 2
+#         print(newstr)
+#         newstr = newstr[1:-1]
+#         print(newstr)
+#         if (s in newstr):
+#             return True
+#         else:
+#             return False
+        
+class Solution:
+    def repeatedSubstringPattern(self, s: str) -> bool:
 
-#         sublen = 1
-#         for i in range(len(s)//2):
+        sublen = 1
+        for i in range(len(s)//2):
 
-#             substr = s[:sublen]
-#             substr = substr * (len(s)//sublen)
-#             if s == substr:
-#                 return True
+            substr = s[:sublen]
+            substr = substr * (len(s)//sublen)
+            if s == substr:
+                return True
             
-#             sublen += 1
+            sublen += 1
 
-#         return False
+        return False
         
             
