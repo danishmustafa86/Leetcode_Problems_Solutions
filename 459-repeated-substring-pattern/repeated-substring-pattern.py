7e770253f8d821003fb7361d3c9a1b29e560f4e2
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-08-30 17:22:57 +0500

    Time: 38 ms (78.94%) | Memory: 16.6 MB (82.01%) - LeetSync

--- 459-repeated-substring-pattern/repeated-substring-pattern.py
+++ 459-repeated-substring-pattern/repeated-substring-pattern.py
@@ -10,19 +10,19 @@ class Solution:
         else:
             return False
         
-class Solution:
-    def repeatedSubstringPattern(self, s: str) -> bool:
+# class Solution:
+#     def repeatedSubstringPattern(self, s: str) -> bool:
 
-        sublen = 1
-        for i in range(len(s)//2):
+#         sublen = 1
+#         for i in range(len(s)//2):
 
-            substr = s[:sublen]
-            substr = substr * (len(s)//sublen)
-            if s == substr:
-                return True
+#             substr = s[:sublen]
+#             substr = substr * (len(s)//sublen)
+#             if s == substr:
+#                 return True
             
-            sublen += 1
+#             sublen += 1
 
-        return False
+#         return False
         
             
