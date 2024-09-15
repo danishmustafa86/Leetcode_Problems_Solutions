5440f2967de5bae1f47ba2425b09a01d6efd30c5
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-09-14 23:35:33 -0700

    Time: 47 ms (47.07%) | Memory: 16.9 MB (90.74%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,43 +1,42 @@
+
 # class Solution:
 #     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-#         rows,cols = len(matrix),len(matrix[0])
-#         top , bot = 0 , cols-1
-#         while top<=bot:
+#         rows, cols = len(matrix), len(matrix[0])
+#         top , bot = 0 , rows-1
+#         while top <= bot:
 #             row = (top + bot) // 2
 #             if target > matrix[row][-1]:
 #                 top = row + 1
 #             elif target < matrix[row][0]:
-#                 bot=row-1
+#                 bot = row - 1
 #             else:
-#                 break
-#         if not (top<=bot):
-#             return False
-#         row = (top+bot)//2
-#         l,r=0,cols-1
-#         while l<=r:
-#             m=(l+r)//2
-#             if target < matrix[row][m]:
-#                 r=m-1
-#             elif target> matrix[row][m]:
-#                 l=m+1
-#             else:
-#                 True
-#         return False
+#                 l, r = 0, cols - 1
+#                 while l <= r:
+#                     m = (l + r) // 2
+#                     if target < matrix[row][m]:
+#                         r = m - 1
+#                     elif target > matrix[row][m]:
+#                         l = m + 1
+#                     else:
+#                         return True  # Return True when target is found
+#                 return False  # Return False if target is not found in the row
+#         return False  # Return False if target is not found in the matrix
+
 
 
 
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        rows, cols = len(matrix), len(matrix[0])
-        top , bot = 0 , rows-1
-        while top <= bot:
-            row = (top + bot) // 2
-            if target > matrix[row][-1]:
+        rows , cols = len(matrix), len(matrix[0])
+        top , bottom = 0, rows - 1
+        while top <= bottom:
+            row = (top + bottom) // 2
+            if target < matrix[row][0]:
+                bottom = row - 1
+            elif target > matrix[row][-1]:
                 top = row + 1
-            elif target < matrix[row][0]:
-                bot = row - 1
             else:
-                l, r = 0, cols - 1
+                l , r = 0, cols - 1
                 while l <= r:
                     m = (l + r) // 2
                     if target < matrix[row][m]:
@@ -45,6 +44,6 @@ class Solution:
                     elif target > matrix[row][m]:
                         l = m + 1
                     else:
-                        return True  # Return True when target is found
-                return False  # Return False if target is not found in the row
-        return False  # Return False if target is not found in the matrix
+                        return True
+                return False
+        return False
