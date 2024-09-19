b2d9cc078b43c11b5b516fddb281ae2b1b162c8e
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-09-19 03:32:24 -0700

    Time: 43 ms (69.65%) | Memory: 19.6 MB (17.15%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
@@ -9,14 +9,28 @@ class Solution:
         n = 0
         stack = []
         cur = root
-        while cur or stack:
+        while root or stack:
             while cur:
                 stack.append(cur)
                 cur = cur.left
             cur = stack.pop()
             n += 1
-            if n  == k:
+            if n == k:
                 return cur.val
             cur = cur.right
+
+
+        # n = 0
+        # stack = []
+        # cur = root
+        # while cur or stack:
+        #     while cur:
+        #         stack.append(cur)
+        #         cur = cur.left
+        #     cur = stack.pop()
+        #     n += 1
+        #     if n  == k:
+        #         return cur.val
+        #     cur = cur.right
         
                 
