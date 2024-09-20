e07e75d739d25deec89da28c650301c1c0e83844
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-09-20 03:25:09 -0700

    Time: 419 ms (28.87%) | Memory: 16.8 MB (91.91%) - LeetSync

--- 214-shortest-palindrome/shortest-palindrome.py
+++ 214-shortest-palindrome/shortest-palindrome.py
@@ -1,6 +1,46 @@
 class Solution:
     def shortestPalindrome(self, s: str) -> str:
 
+        for i in range(len(s), 0,-1):
+            front = s[:i]
+            rev = front[::-1]
+            cur = s[i:]
+            if front == rev:
+                return cur[::-1] + s
+        return ""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         rev = s[::-1]
         for i in range(len(s) + 1):
             if s.startswith(rev[i:]):
