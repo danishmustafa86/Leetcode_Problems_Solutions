735248204c7d3fd3d2dec063e9b5379c48375813
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-09-29 13:50:18 +0500

    Time: 57 ms (42.23%) | Memory: 16.6 MB (77.23%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -5,24 +5,33 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-
-        dummy = ListNode(0)
-        cur = dummy
-
-        carry  = 0
-        while l1 or l2 or carry:
-            v1 = l1.val if l1 else 0
-            v2 = l2.val if l2 else 0
-
-            val = v1 + v2 + carry
-            carry = val//10
-            val = val%10
-            cur.next = ListNode(val)
-
-            l1 = l1.next if l1 else None
-            l2 = l2.next if l2 else None
+        n1, n2 = [], []
+        cur = l1
+        while cur:
+            n1.append(str(cur.val))
+            cur = cur.next
+        cur = l2
+        while cur:
+            n2.append(str(cur.val))
             cur = cur.next
         
+        n1.reverse()
+        n1 = "".join(n1)
+        n2.reverse()
+        n2 = "".join(n2)
+        sm = int(n1) + int(n2)
+        sm = str(sm)
+        sm = list(sm)
+        # sm.reverse()
+        sm = "".join(sm)
+        ans = []
+        for i in range(len(sm)-1, -1, -1):
+            ans.append(int(sm[i]))
+        
+        dummy = ListNode()
+        tail = dummy
+        for i in range(len(ans)):
+            tail.next = ListNode(int(ans[i]))
+            tail = tail.next
         return dummy.next
-
 
