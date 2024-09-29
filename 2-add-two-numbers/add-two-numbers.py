004ffd377cd67634d22644e6af047f2e9ae373f3
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-09-29 14:26:17 +0500

    Time: 66 ms (5.48%) | Memory: 16.7 MB (31.51%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -5,33 +5,22 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        n1, n2 = [], []
-        cur = l1
-        while cur:
-            n1.append(str(cur.val))
-            cur = cur.next
-        cur = l2
-        while cur:
-            n2.append(str(cur.val))
-            cur = cur.next
-        
-        n1.reverse()
-        n1 = "".join(n1)
-        n2.reverse()
-        n2 = "".join(n2)
-        sm = int(n1) + int(n2)
-        sm = str(sm)
-        sm = list(sm)
-        # sm.reverse()
-        sm = "".join(sm)
-        ans = []
-        for i in range(len(sm)-1, -1, -1):
-            ans.append(int(sm[i]))
-        
+   
         dummy = ListNode()
-        tail = dummy
-        for i in range(len(ans)):
-            tail.next = ListNode(int(ans[i]))
-            tail = tail.next
+        cur   = dummy 
+        carry = 0
+        while l1 or l2:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
+            sm = v1 + v2 + carry
+            carry = sm // 10
+            cur.next = ListNode(sm % 10)
+            cur = cur.next
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+        if carry:
+            cur.next = ListNode(carry)
         return dummy.next
-
+            
