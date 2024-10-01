489fbf842e923786579092c6c713af12b7bd3e9a
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-10-01 19:55:40 +0500

    Time: 35 ms (61.29%) | Memory: 16.5 MB (44.88%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.py
+++ 1127-last-stone-weight/last-stone-weight.py
@@ -1,33 +1,37 @@
-class Solution:
-    def lastStoneWeight(self, stones: List[int]) -> int:
-        stones = [-s for s in stones]
-        heapq.heapify(stones)
-        while len(stones) > 1:
-            first = heapq.heappop(stones)
-            second = heapq.heappop(stones)
-            if second > first:
-                heapq.heappush(stones, first - second)
-        stones.append(0)
-        return abs(stones[0])
-
-
-
-
-
-
-
-
 # class Solution:
 #     def lastStoneWeight(self, stones: List[int]) -> int:
-        # stones.sort()
+
+
+
+        # stones = [-s for s in stones]
+        # heapq.heapify(stones)
         # while len(stones) > 1:
-        #     if stones[-1] == stones[-2]:
-        #         stones.pop()
-        #         stones.pop()
-        #     else:
-        #         v1  = stones.pop()
-        #         v2  = stones.pop()
-        #         stones.append(v1 - v2)
-        #         stones.sort()
-        # return stones[0] if stones[0] != 0 else 0
+        #     first = heapq.heappop(stones)
+        #     second = heapq.heappop(stones)
+        #     if second > first:
+        #         heapq.heappush(stones, first - second)
+        # stones.append(0)
+        # return abs(stones[0])
+
+
+
+
+
+
+
+
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        stones.sort()
+        while len(stones) > 1:
+            if stones[-1] == stones[-2]:
+                stones.pop()
+                stones.pop()
+            else:
+                v1  = stones.pop()
+                v2  = stones.pop()
+                stones.append(v1 - v2)
+                stones.sort()
+        stones.append(0)
+        return stones[0] 
 
