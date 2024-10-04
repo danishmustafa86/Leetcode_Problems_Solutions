541cf38d2c95e7a6254332d46d0170167183deaf
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-10-04 15:55:06 +0500

    Time: 592 ms (67.39%) | Memory: 22.2 MB (94.42%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -1,15 +1,18 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        # heap = []
-        # for val in points:
-        #     cur = val[0]**2 + val[1]**2
-        #     heapq.heappush(heap, (cur, val))
-        # result = [heapq.heappop(heap)[1] for _ in range(k)]
-        # return result
-
         heap = []
         for val in points:
             cur = val[0]**2 + val[1]**2
-            heapq.heappush(heap, (cur,val))
-        result = [heapq.heappop(heap)[1] for _ in range(k)]
+            heapq.heappush(heap, ( cur, val))
+        result = [ heapq.heappop(heap)[1] for _ in range(k)]
         return result
+
+
+
+        # heap = []
+        # for val in points:
+        #     cur = val[0]**2 + val[1]**2
+        #     heapq.heappush(heap, (cur,val))
+        # print(heap)
+        # result = [heapq.heappop(heap)[1] for _ in range(k)]
+        # return result
