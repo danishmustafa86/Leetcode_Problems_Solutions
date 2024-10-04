7bb5c8e19e24e2258c4e8faafd2efabbc0bfa670
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-10-04 15:25:25 +0500

    Time: 100 ms (39.65%) | Memory: 22.2 MB (18.18%) - LeetSync

--- 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.py
+++ 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.py
@@ -1,33 +1,55 @@
 class KthLargest:
 
     def __init__(self, k: int, nums: List[int]):
-        self.minHeap, self.k = nums, k
-        heapq.heapify(self.minHeap)
-        while len(self.minHeap) > self.k:
-            heapq.heappop(self.minHeap)
+        self.maxHeap, self.k = nums, k
+        heapq.heapify(self.maxHeap)
+        while len(self.maxHeap) > self.k:
+            heapq.heappop(self.maxHeap)
 
     def add(self, val: int) -> int:
-        heapq.heappush(self.minHeap, val)
-        if len(self.minHeap) > self.k:
-            heapq.heappop(self.minHeap)
-        return self.minHeap[0]
+        heapq.heappush(self.maxHeap, val)
+        if len(self.maxHeap) > self.k:
+            heapq.heappop(self.maxHeap)
+        return self.maxHeap[0]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # def __init__(self, k: int, nums: List[int]):
+    #     self.minHeap, self.k = nums, k
+    #     heapq.heapify(self.minHeap)
+    #     while len(self.minHeap) > self.k:
+    #         heapq.heappop(self.minHeap)
+
+    # def add(self, val: int) -> int:
+    #     heapq.heappush(self.minHeap, val)
+    #     if len(self.minHeap) > self.k:
+    #         heapq.heappop(self.minHeap)
+    #     return self.minHeap[0]
         
-# class KthLargest:
-
-#     def __init__(self, k: int, nums: List[int]):
-#         self.minHeap, self.k = nums, k
-#         heapq.heapify(self.minHeap)
-#         while len(self.minHeap) > k:
-#             heapq.heappop(self.minHeap)        
-
-#     def add(self, val: int) -> int:
-#         heapq.heappush(self.minHeap,val)
-#         if len(self.minHeap) > self.k:
-#             heapq.heappop(self.minHeap)
-#         return self.minHeap[0]
-        
-
-
-# Your KthLargest object will be instantiated and called as such:
-# obj = KthLargest(k, nums)
-# param_1 = obj.add(val)
