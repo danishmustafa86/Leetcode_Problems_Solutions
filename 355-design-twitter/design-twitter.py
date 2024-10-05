127d7e1c2355b92b04c43878e6f6c95df6ffc2e4
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-10-05 14:15:39 +0500

    Time: 30 ms (87.14%) | Memory: 16.9 MB (11.08%) - LeetSync

---
+++ 355-design-twitter/design-twitter.py
@@ -0,0 +1,45 @@
+class Twitter:
+
+    def __init__(self):
+        
+        self.count = 0
+        self.followMap = defaultdict(set)
+        self.tweetMap = defaultdict(list)
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        self.tweetMap[userId].append([tweetId,self.count])
+        self.count -= 1
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        minHeap = []
+        res = []
+
+        self.followMap[userId].add(userId)
+        for followeeId in self.followMap[userId]:
+            if followeeId in self.tweetMap:
+                index = len(self.tweetMap[followeeId]) - 1
+                tweetId, count = self.tweetMap[followeeId][index]
+                minHeap.append([count, tweetId, followeeId, index - 1])
+        heapq.heapify(minHeap)
+        while minHeap and len(res) < 10:
+            count, tweetId, followeeId, index = heapq.heappop(minHeap)
+            res.append(tweetId)
+            if index >= 0 :
+                tweetId, count = self.tweetMap[followeeId][index]
+                heapq.heappush(minHeap, [count, tweetId, followeeId, index - 1])
+        return res
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        self.followMap[followerId].add(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+            if  followeeId in   self.followMap[followerId]:
+                self.followMap[followerId].remove(followeeId)
+
+
+# Your Twitter object will be instantiated and called as such:
+# obj = Twitter()
+# obj.postTweet(userId,tweetId)
+# param_2 = obj.getNewsFeed(userId)
+# obj.follow(followerId,followeeId)
+# obj.unfollow(followerId,followeeId)
