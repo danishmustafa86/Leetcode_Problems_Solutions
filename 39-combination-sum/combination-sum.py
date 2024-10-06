9dbf98ef96398a5fb151e6dc5b170420d1b48d6c
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-10-06 11:18:29 +0500

    Time: 55 ms (44.53%) | Memory: 16.7 MB (14.64%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,24 +1,20 @@
 class Solution:
-
-
-
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        temp = []
-        ans = []
-        def recursion(candidate, target, i , curSum, temp):
+        def recursion(candidates, target, i, curSum, temp, ans):
             if curSum > target:
                 return
-            if i == len(candidate):
-                if curSum == target:
-                    ans.append(temp[:])
+            if curSum == target:
+                ans.append(temp[:])  # Append a copy of temp
+                return
+            if i == len(candidates):
                 return
             # Inclusion
-            curSum += candidate[i]
-            temp.append(candidate[i])
-            recursion(candidate, target, i, curSum, temp)
-            curSum -= candidate[i]
-            temp.pop()
+            temp.append(candidates[i])
+            recursion(candidates, target, i, curSum + candidates[i], temp, ans)
+            temp.pop()  # Remove the last element to backtrack
             # Exclusion
-            recursion(candidate, target, i+1, curSum, temp)
-        recursion(candidates, target, 0 , 0 , temp)
+            recursion(candidates, target, i + 1, curSum, temp, ans)
+
+        ans = []
+        recursion(candidates, target, 0, 0, [], ans)
         return ans
