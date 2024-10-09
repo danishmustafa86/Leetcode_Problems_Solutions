353076d1db7abb9f46be4ffed46d861f43464fd7
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-10-09 10:54:10 +0500

    Time: 39 ms (66.86%) | Memory: 16.6 MB (73.67%) - LeetSync

---
+++ 46-permutations/permutations.py
@@ -0,0 +1,13 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+
+        if len(nums) == 0:
+            return [[]]
+        perms = self.permute(nums[1:])
+        res = []
+        for p in perms:
+            for i in range(len(p) + 1):
+                p_copy = p.copy()
+                p_copy.insert(i, nums[0])
+                res.append(p_copy)
+        return res
