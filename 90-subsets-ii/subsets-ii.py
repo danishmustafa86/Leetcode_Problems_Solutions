d39d71225a7d510d350755f9339db56d1c4fd4f4
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-10-11 22:26:35 +0500

    Time: 46 ms (15.14%) | Memory: 16.9 MB (17.44%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.py
@@ -0,0 +1,29 @@
+from typing import List
+
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        temp = []
+        nums.sort()  # Sort the input list to handle duplicates
+        
+        def dfs(i):
+            if i == len(nums):
+                # Append a copy of temp (as tuple) to avoid duplicate subsets
+                if temp not in res:
+                    res.append(temp[:])
+                return 
+            
+            # Include the current number in the subset
+            temp.append(nums[i])
+            dfs(i + 1)
+            
+            # Backtrack and do not include the current number
+            temp.pop()
+            dfs(i + 1)
+
+        dfs(0)
+        return res
+
+# Example usage:
+# sol = Solution()
+# print(sol.subsetsWithDup([1, 2, 2]))
