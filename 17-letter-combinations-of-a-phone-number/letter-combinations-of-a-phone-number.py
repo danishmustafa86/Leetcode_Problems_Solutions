74d8188a537da388b64d8ff7602ab235797d58ea
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-10-17 19:20:12 +0500

    Time: 0 ms (100.00%) | Memory: 16.6 MB (18.68%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,45 +1,33 @@
-# class Solution:
-#     def letterCombinations(self, digits: str) -> List[str]:
-#         hsh = {2:"abc",3:"def",4:"ghi",5:"jkl",6:"mno",7:"pqrs",8:"tuv",9:"wxyz"}
-#         n = 0
-#         l = len(digits)
-#         if not digits :
-#             return []
-#         if l == 1 :
-#             n = int(digits)
-#             return list(hsh[n])
-#         old = [""] 
-#         for i in digits:
-#             new = []
-
-#             # n = int(digits[i])
-#             # cur = list(hsh[n])
-#             for j in old:
-#                 for k in hsh[int(i)]:
-#                     new.append(j + k)
-#             old = new
-                
-#         return new
-
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        hsh = {2:"abc",3:"def",4:"ghi",5:"jkl",6:"mno",7:"pqrs",8:"tuv",9:"wxyz"}
-        n = 0
-        l = len(digits)
-        if not digits :
-            return []
-        if l == 1 :
-            n = int(digits)
-            return list(hsh[n])
-        old =  [""]
-        for i in range(l - 1,-1,-1):
-            new = []
+        res = []
+        digitsToChar = {
+            "2": "abc", "3": "def", "4": "ghi", "5": "jkl", "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"
+        }
+        def dfs(i, curStr):
+            if len(curStr) == len(digits):
+                res.append(curStr)
+                return
+            for c in digitsToChar[digits[i]]:
+                dfs( i + 1, curStr + c)
+        
+        if digits:
+            dfs(0, "")
+        
+        return res
 
-            n = int(digits[i])
-            cur = list(hsh[n])
-            for j in cur:
-                for k in old:
-                    new.append(j + k)
-            old = new
-                
-        return new
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
