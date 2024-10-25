3fc245a905e26bed12babc50732ed6259394905d
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-10-25 21:41:39 +0500

    Time: 7188 ms (6.48%) | Memory: 17 MB (99.51%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -1,10 +1,10 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        arr = []
         p = sorted(p)
-        pl = len(p)
-        n = len(s)
-        for i in range(n-pl+1):
-            if sorted(s[i:i+pl]) == p :
+        arr = []
+        for i in range(len(s) - len(p) + 1 ):
+            if sorted(s[i:i+ len(p)]) == p :
                 arr.append(i)
+
         return arr
+
