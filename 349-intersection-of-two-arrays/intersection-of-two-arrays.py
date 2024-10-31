c66a0f758184d496247c8d2ce2d69d6221355eb3
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-10-31 21:47:34 +0500

    Time: 1 ms (42.83%) | Memory: 16.8 MB (42.71%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.py
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.py
@@ -9,8 +9,67 @@ class Solution:
                 intersection.append(nums1[i])
                 i += 1
                 j += 1
-            elif nums1[i] < nums2[j]:
-                i += 1
-            else:
+            elif nums1[i] > nums2[j]:
                 j += 1
+            else:
+                i += 1
         return intersection
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # nums1.sort()
+        # nums2.sort()
+        # intersection = []
+        # i, j = 0, 0
+        # while i < len(nums1) and j < len(nums2):
+        #     if nums1[i] == nums2[j] and nums1[i] not in intersection:
+        #         intersection.append(nums1[i])
+        #         i += 1
+        #         j += 1
+        #     elif nums1[i] < nums2[j]:
+        #         i += 1
+        #     else:
+        #         j += 1
+        # return intersection
