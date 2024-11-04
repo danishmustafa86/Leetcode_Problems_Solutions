caa53f663cfae79fc8e5c5459c756f4ad22497f9
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-11-04 21:01:37 +0500

    Time: 51 ms (80.29%) | Memory: 18.7 MB (90.94%) - LeetSync

--- 713-subarray-product-less-than-k/subarray-product-less-than-k.py
+++ 713-subarray-product-less-than-k/subarray-product-less-than-k.py
@@ -1,21 +1,48 @@
 class Solution:
     def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
-        if k <= 1:
-            return 0  # Edge case where k is too small to have any valid subarray
-
+        if k <= 0:
+            return 0
         product = 1
-        left = 0
+        left  = 0
         res = 0
-
         for right in range(len(nums)):
             product *= nums[right]
-
-            # Shrink the window from the left while the product is greater or equal to k
             while product >= k and left <= right:
                 product //= nums[left]
                 left += 1
-
-            # The number of subarrays ending at `right` is (right - left + 1)
             res += right - left + 1
-
         return res
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Solution:
+#     def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+#         if k <= 1:
+#             return 0  
+
+#         product = 1
+#         left = 0
+#         res = 0
+
+#         for right in range(len(nums)):
+#             product *= nums[right]
+
+         
+#             while product >= k and left <= right:
+#                 product //= nums[left]
+#                 left += 1
+
+#             res += right - left + 1
+
+#         return res
