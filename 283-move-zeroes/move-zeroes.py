84bb1e38638ee688dfacaa923c5ae52af8813794
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-11-06 20:55:14 +0500

    Time: 7 ms (46.51%) | Memory: 17.8 MB (72.95%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -6,17 +6,6 @@ class Solution:
                 left += 1
                 continue
             if nums[right] != 0:
-                nums[right], nums[left] = nums[left], nums[right]
+                nums[left], nums[right] = nums[right], nums[left]
                 left += 1
-        return nums
-# class Solution:
-#     def moveZeroes(self, nums: List[int]) -> None:
-#         left = 0
-#         for right in range(1,len(nums)):
-#             if nums[left] != 0:
-#                 left += 1
-#                 continue
-#             if nums[right] != 0:
-#                 nums[right], nums[left] = nums[left] , nums[right]
-#                 left += 1
-#         return nums
+        # return nums
