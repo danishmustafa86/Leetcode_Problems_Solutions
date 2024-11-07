298815105e88711d2375a9ffb8fefc6694b90ae9
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-11-07 20:42:55 +0500

    Time: 3 ms (47.18%) | Memory: 16.6 MB (51.56%) - LeetSync

--- 1666-make-the-string-great/make-the-string-great.py
+++ 1666-make-the-string-great/make-the-string-great.py
@@ -2,8 +2,28 @@ class Solution:
     def makeGood(self, s: str) -> str:
         stk = []
         for val in s:
-            if stk and stk[-1] != val and val.lower() == stk[-1].lower():
+            if stk and stk[-1] != val and stk[-1].lower() == val.lower():
                 stk.pop()
             else:
                 stk.append(val)
         return "".join(stk)
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # stk = []
+        # for val in s:
+        #     if stk and stk[-1] != val and val.lower() == stk[-1].lower():
+        #         stk.pop()
+        #     else:
+        #         stk.append(val)
+        # return "".join(stk)
