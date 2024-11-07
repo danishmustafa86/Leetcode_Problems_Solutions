5e0ee43e0e3d0a920c7344d32a63e1655ac1ac38
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-11-07 21:01:19 +0500

    Time: 2 ms (27.75%) | Memory: 16.5 MB (75.28%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -2,17 +2,31 @@ class Solution:
     def isValid(self, s: str) -> bool:
         st1 = "([{"
         st2 = ")]}"
-        matching_brackets = {')': '(', ']': '[', '}': '{'}
-        stack = []
-        
+        stk = []
+        match_brackets = { ")":"(", "]":"[", "}":"{"}
         for i in s:
-            if i in st1:  # If it's an opening bracket, push to stack
-                stack.append(i)
-            elif i in st2:  # If it's a closing bracket
-                # Check if stack is empty or top of stack doesn't match
-                if not stack or stack[-1] != matching_brackets[i]:
+            if i in st1:
+                stk.append(i)
+            elif i in st2:
+                if not stk or stk[-1] != match_brackets[i]:
                     return False
-                stack.pop()  # Pop the matching opening bracket
+                stk.pop()
+        return len(stk) == 0
+                    
+
+        # st1 = "([{"
+        # st2 = ")]}"
+        # matching_brackets = {')': '(', ']': '[', '}': '{'}
+        # stack = []
         
-        # Check if the stack is empty (all brackets matched)
-        return len(stack) == 0
+        # for i in s:
+        #     if i in st1:  # If it's an opening bracket, push to stack
+        #         stack.append(i)
+        #     elif i in st2:  # If it's a closing bracket
+        #         # Check if stack is empty or top of stack doesn't match
+        #         if not stack or stack[-1] != matching_brackets[i]:
+        #             return False
+        #         stack.pop()  # Pop the matching opening bracket
+        
+        # # Check if the stack is empty (all brackets matched)
+        # return len(stack) == 0
