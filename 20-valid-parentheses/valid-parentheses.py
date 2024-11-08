1594d24a4e5cbe6a84ec82a2ffa014e1a49ea213
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-11-08 20:28:19 +0500

    Time: 0 ms (100.00%) | Memory: 16.6 MB (38.06%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,32 +1,51 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        st1 = "([{"
-        st2 = ")]}"
-        stk = []
-        match_brackets = { ")":"(", "]":"[", "}":"{"}
+        hsh = { ")":"(", "]":"[", "}":"{"}
+        stack = []
         for i in s:
-            if i in st1:
-                stk.append(i)
-            elif i in st2:
-                if not stk or stk[-1] != match_brackets[i]:
+            if i in hsh:
+                if stack and stack[-1] == hsh[i]:
+                    stack.pop()
+                else:
                     return False
-                stk.pop()
-        return len(stk) == 0
-                    
 
-        # st1 = "([{"
-        # st2 = ")]}"
-        # matching_brackets = {')': '(', ']': '[', '}': '{'}
-        # stack = []
+            else:
+                stack.append(i)
+
+        return not stack
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         
-        # for i in s:
-        #     if i in st1:  # If it's an opening bracket, push to stack
-        #         stack.append(i)
-        #     elif i in st2:  # If it's a closing bracket
-        #         # Check if stack is empty or top of stack doesn't match
-        #         if not stack or stack[-1] != matching_brackets[i]:
-        #             return False
-        #         stack.pop()  # Pop the matching opening bracket
-        
-        # # Check if the stack is empty (all brackets matched)
-        # return len(stack) == 0
+# class Solution:
+#     def isValid(self, s: str) -> bool:
+#         st1 = "([{"
+#         st2 = ")]}"
+#         stk = []
+#         match_brackets = { ")":"(", "]":"[", "}":"{"}
+#         for i in s:
+#             if i in st1:
+#                 stk.append(i)
+#             elif i in st2:
+#                 if not stk or stk[-1] != match_brackets[i]:
+#                     return False
+#                 stk.pop()
+#         return len(stk) == 0
+                    
