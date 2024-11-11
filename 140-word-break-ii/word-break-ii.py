c76b4f768f5aa04c065f742a1756704fd567bc24
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-11-11 17:40:17 +0500

    Time: 0 ms (100.00%) | Memory: 16.7 MB (7.69%) - LeetSync

--- 140-word-break-ii/word-break-ii.py
+++ 140-word-break-ii/word-break-ii.py
@@ -1,16 +1,46 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        wordDict = set(wordDict)
+        cache = {}
         def backtrack(i):
             if i == len(s):
-                return res.append(" ".join(cur))
+                return [""]
+            if i in cache:
+                return cache[i]
+                
+            res = []
             for j in range(i, len(s)):
-                curSt = s[i:j+1]
-                if curSt in wordDict:
-                    cur.append(curSt)
-                    backtrack(j + 1)
-                    cur.pop()
+                w = s[i:j+1]
+                if w not in wordDict:
+                    continue
+                string = backtrack(j+1)
+                # if not string:
+                #     continue
+                for substr in string:
+                    sentence = w
+                    if substr:
+                        sentence += " " + substr
+                    res.append(sentence)
+            cache[i] = res
+            return res
+
+        return backtrack(0)
+        
+
+
+# class Solution:
+#     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+#         def backtrack(i):
+#             if i == len(s):
+#                 return res.append(" ".join(cur))
+#             for j in range(i, len(s)):
+#                 curSt = s[i:j+1]
+#                 if curSt in wordDict:
+#                     cur.append(curSt)
+#                     backtrack(j + 1)
+#                     cur.pop()
                     
-        cur = []
-        res = []
-        backtrack(0)
-        return res
+#         cur = []
+#         res = []
+#         backtrack(0)
+#         return res
