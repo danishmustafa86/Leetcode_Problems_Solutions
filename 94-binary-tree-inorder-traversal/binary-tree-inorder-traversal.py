b0ecaa8a806cf1f00d5a57c4a2c580de09e3a217
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-11-13 16:54:56 +0500

    Time: 0 ms (100.00%) | Memory: 16.6 MB (59.14%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.py
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.py
@@ -6,51 +6,12 @@
 #         self.right = right
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        res = []  # This list will store the inorder traversal
-        def rec(node):
-            if node:
-                rec(node.left)    # Traverse left subtree
-                res.append(node.val)  # Visit node
-                rec(node.right)   # Traverse right subtree
-        rec(root)  # Start the recursion with the root node
-        return res  # Return the result
-
-
-
-
-
-
-
-
-
-        # stack = []
-        # inorder = []
-        # while (stack or inorder):
-        #     if root:
-        #         stack.append(root)
-        #         root = root.left
-        #     else:
-        #         root = stack.pop()
-        #         inorder.append(root.val)
-        #         root = root.right
-        # return inorder
-
-
-
-
-
-
-
-
-
-
-        # st=[]
-        # res=[]
-        # while root or st:
-        #     while root:
-        #         st.append(root)
-        #         root=root.left
-        #     root=st.pop()
-        #     res.append(root.val)
-        #     root=root.right
-        # return res
+        res = []
+        def inorder(root):
+            if not root:
+                return 
+            inorder(root.left)
+            res.append(root.val)
+            inorder(root.right)
+        inorder(root)
+        return res
