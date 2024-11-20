c5660964c0fe66683b589c5bf0513a795953b288
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-11-20 19:54:19 +0500

    Time: 80 ms (93.44%) | Memory: 48.3 MB (91.57%) - LeetSync

--- 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.py
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.py
@@ -5,19 +5,56 @@
 #         self.next = next
 class Solution:
     def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        def reverse(head):
+            prev, cur = None, head
+            while cur:
+                tmp = cur.next
+                cur.next = prev
+                prev, cur = cur, tmp
+            return prev
+
+        head = reverse(head)
+
         cur = head
-        stack = []
-        while cur:
-            while stack and stack[-1] < cur.val:
-                stack.pop()
-            stack.append(cur.val)
-            cur = cur.next
+        max_val = cur.val
+        while cur.next:
+            if cur.next.val < max_val:
+                cur.next = cur.next.next
+            else:
+                max_val = cur.next.val
+                cur = cur.next
+
+
+        return reverse(head)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Solution:
+#     def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+#         cur = head
+#         stack = []
+#         while cur:
+#             while stack and stack[-1] < cur.val:
+#                 stack.pop()
+#             stack.append(cur.val)
+#             cur = cur.next
         
-        dummy = ListNode()
-        cur = dummy
-        for val in stack:
-            cur.next = ListNode(val)
-            cur = cur.next
+#         dummy = ListNode()
+#         cur = dummy
+#         for val in stack:
+#             cur.next = ListNode(val)
+#             cur = cur.next
         
-        return dummy.next
+#         return dummy.next
         
