e357dcdc467458aa4d301f1035796c0dee560cc0
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-11-25 16:30:11 +0500

    Time: 39 ms (72.16%) | Memory: 17.2 MB (52.93%) - LeetSync

--- 463-island-perimeter/island-perimeter.py
+++ 463-island-perimeter/island-perimeter.py
@@ -1,24 +1,16 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
 
-        rows = len(grid)
-        cols = len(grid[0])
         perimeter = 0
-        
-        for r in range(rows):
-            for c in range(cols):
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
                 if grid[r][c] == 1:
-                    # Check the top neighbor
-                    if r == 0 or grid[r-1][c] == 0:
-                        perimeter += 1
-                    # Check the bottom neighbor
-                    if r == rows-1 or grid[r+1][c] == 0:
-                        perimeter += 1
-                    # Check the left neighbor
                     if c == 0 or grid[r][c-1] == 0:
                         perimeter += 1
-                    # Check the right neighbor
-                    if c == cols-1 or grid[r][c+1] == 0:
+                    if c == len(grid[0]) - 1 or grid[r][ c + 1] == 0:
                         perimeter += 1
-                        
+                    if r == 0 or grid[r - 1][c] == 0:
+                        perimeter += 1
+                    if r == len(grid) - 1 or grid[r + 1][c] == 0:
+                        perimeter += 1
         return perimeter
