8a802238f3ec1d7081482aafd2befa311f613829
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-11-26 16:40:26 +0500

    Time: 25 ms (23.57%) | Memory: 20.5 MB (40.30%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.py
+++ 1039-find-the-town-judge/find-the-town-judge.py
@@ -1,19 +1,13 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        hsh = { trust[i][0]:trust[i][1] for i in range(len(trust))}
-        temp = 0
 
-
-
+        hsh = defaultdict(int)
+        for f, s in trust:
+            hsh[f] -= 1
+            hsh[s] += 1
         for i in range(1, n+1):
-            if i not in hsh:
-                for j in range(len(trust)):
-                    if i == trust[j][1]:
-                        temp += 1
-                if temp == n - 1:
-                    return i
-            temp = 0
-
+            if hsh[i] == n - 1:
+                return i
         return -1
 
 
