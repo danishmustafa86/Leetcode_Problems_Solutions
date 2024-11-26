c30e73153b8ec90334591c9522f7cf578907ffe3
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-11-26 19:32:22 +0500

    Time: 1 ms (23.71%) | Memory: 17.3 MB (6.08%) - LeetSync

--- 61-rotate-list/rotate-list.py
+++ 61-rotate-list/rotate-list.py
@@ -5,24 +5,59 @@
 #         self.next = next
 class Solution:
     def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        if not head or not head.next or k == 0:
+
+        if not head:
             return head
-        arr = []
-        cur  = head
-        while cur:
-            arr.append(cur.val)
-            cur = cur.next
-        k = k % len(arr)
+        tail, length = head, 1
+        while tail.next:
+            length += 1
+            tail = tail.next
+        k = k % length
         if k == 0:
             return head
-        arr = arr[-k:] + arr[:-k]
+        
+        cur = head
+        for i in range(length - k - 1):
+            cur = cur.next
+        newHead = cur.next
+        cur.next = None
+        tail.next = head
+        return newHead
 
-        dummy = ListNode()
-        temp = dummy
-        for val in arr:
-            temp.next = ListNode(val)
-            temp = temp.next
-        return dummy.next
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # if not head or not head.next or k == 0:
+        #     return head
+        # arr = []
+        # cur  = head
+        # while cur:
+        #     arr.append(cur.val)
+        #     cur = cur.next
+        # k = k % len(arr)
+        # if k == 0:
+        #     return head
+        # arr = arr[-k:] + arr[:-k]
+        # dummy = ListNode()
+        # temp = dummy
+        # for val in arr:
+        #     temp.next = ListNode(val)
+        #     temp = temp.next
+        # return dummy.next
 
 
 
