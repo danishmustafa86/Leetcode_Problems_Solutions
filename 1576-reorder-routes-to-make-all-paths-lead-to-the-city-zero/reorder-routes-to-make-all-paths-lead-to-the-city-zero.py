eb7b7a56683e21b1864e84fafa7405ecd9c13e6e
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-11-30 13:57:25 +0500

    Time: 175 ms (71.73%) | Memory: 52.8 MB (16.42%) - LeetSync

--- 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
@@ -1,16 +1,16 @@
 class Solution:
     def minReorder(self, n: int, connections: List[List[int]]) -> int:
         
-        edges = { (a,b) for a,b in connections}
-        neighbours = { city:[] for city in range(n)}
         visit = set()
         changes = 0
+        neighbours = { city:[] for city in range(n)}
+        edges = { (a,b) for a,b in connections}
 
         for a,b in connections:
             neighbours[a].append(b)
             neighbours[b].append(a)
         def dfs(city):
-            nonlocal edges, neighbours, visit, changes
+            nonlocal changes, visit, neighbours, edges
             for neighbour in neighbours[city]:
                 if neighbour in visit:
                     continue
@@ -18,6 +18,8 @@ class Solution:
                     changes += 1
                 visit.add(neighbour)
                 dfs(neighbour)
+
+
         visit.add(0)
         dfs(0)
         return changes
