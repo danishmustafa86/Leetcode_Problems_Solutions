a22b20b58abb113eccc5f0ef772ba8e74f08f876
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-12-05 21:40:23 +0500

    Time: 0 ms (100.00%) | Memory: 17.5 MB (8.75%) - LeetSync

--- 107-binary-tree-level-order-traversal-ii/binary-tree-level-order-traversal-ii.py
+++ 107-binary-tree-level-order-traversal-ii/binary-tree-level-order-traversal-ii.py
@@ -6,21 +6,77 @@
 #         self.right = right
 class Solution:
     def levelOrderBottom(self, root: Optional[TreeNode]) -> List[List[int]]:
+
+
+
         res = []
         q = deque()
         q.append(root)
+        n = 0
         while q:
             level = []
+            n += 1
             for i in range(len(q)):
                 cur = q.popleft()
                 if cur:
+                    level.append(cur.val)
                     q.append(cur.left)
                     q.append(cur.right)
-                    level.append(cur.val)
             if level:
                 res.append(level)
-        # ans = []
-        # for i in range(len(res) - 1, -1, -1):
-        #     ans.append(res[i])
-        cur = res.reverse()
+        res.reverse()                
+
         return res
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # res = []
+        # q = deque()
+        # q.append(root)
+        # while q:
+        #     level = []
+        #     for i in range(len(q)):
+        #         cur = q.popleft()
+        #         if cur:
+        #             q.append(cur.left)
+        #             q.append(cur.right)
+        #             level.append(cur.val)
+        #     if level:
+        #         res.append(level)
+        # res.reverse()
+        # return res
