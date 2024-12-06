4d948827837e283fe992fef84431ba5cca76893c
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-12-06 18:00:29 +0500

    Time: 56 ms (12.33%) | Memory: 19 MB (6.38%) - LeetSync

--- 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.py
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.py
@@ -10,24 +10,57 @@ class Node:
 
 class Solution:
     def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+
         if not root:
             return None
         q = deque([root])
         while q:
-            innerQ = deque()
+            q2 = deque()
             for i in range(len(q)):
                 cur = q.popleft()
-                innerQ.append(cur)
+                q2.append(cur)
                 if cur.left:
                     q.append(cur.left)
                 if cur.right:
                     q.append(cur.right)
-            while len(innerQ) > 1:
-                cur = innerQ.popleft()
-                cur.next = innerQ[0]
-            cur = innerQ.popleft()
+            while len(q2) > 1:
+                cur = q2.popleft()
+                cur.next = q2[0]
+            cur = q2.popleft()
             cur.next = None
         return root
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -35,20 +68,22 @@ class Solution:
         #     return None
         # q = deque([root])
         # while q:
-        #     dq = deque()
+        #     innerQ = deque()
         #     for i in range(len(q)):
-        #         node = q.popleft()
-        #         dq.append(node)
-        #         if node.left:
-        #             q.append(node.left)
-        #         if node.right:
-        #             q.append(node.right)
-        #     while len(dq)>1:
-        #         cur  = dq.popleft()
-        #         cur.next = dq[0]
-        #     cur = dq.popleft()
+        #         cur = q.popleft()
+        #         innerQ.append(cur)
+        #         if cur.left:
+        #             q.append(cur.left)
+        #         if cur.right:
+        #             q.append(cur.right)
+        #     while len(innerQ) > 1:
+        #         cur = innerQ.popleft()
+        #         cur.next = innerQ[0]
+        #     cur = innerQ.popleft()
         #     cur.next = None
         # return root
+
+
 
 
 
