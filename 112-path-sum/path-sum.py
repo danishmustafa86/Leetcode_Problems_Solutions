4d41cffabea949f528eac443e863686abbc965bb
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-12-09 20:47:18 +0500

    Time: 0 ms (100.00%) | Memory: 18.3 MB (15.80%) - LeetSync

--- 112-path-sum/path-sum.py
+++ 112-path-sum/path-sum.py
@@ -8,26 +8,26 @@ class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
         if not root:
             return False
-        def traverse (cur:Optional[TreeNode],sum)->bool:
-            if not cur:
-                return False    
-            sum+=cur.val
-            if not cur.left and not cur.right:
-                return sum==targetSum
-            return traverse(cur.left, sum) or traverse(cur.right, sum)
+        def traverse(root, curSum):
+            if not root:
+                return False
+            curSum += root.val
+            if not root.left and not root.right:
+                return curSum == targetSum
+            return traverse(root.left, curSum) or traverse(root.right, curSum)
         return traverse(root,0)
+
 
 
 # class Solution:
 #     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
 #         if not root:
 #             return False
-#         def traverse(cur:Optional[TreeNode], sum) -> bool:
-#             # base condition
+#         def traverse (cur:Optional[TreeNode],sum)->bool:
 #             if not cur:
-#                 return False
-#             sum += cur.val
+#                 return False    
+#             sum+=cur.val
 #             if not cur.left and not cur.right:
-#                 return sum == targetSum
+#                 return sum==targetSum
 #             return traverse(cur.left, sum) or traverse(cur.right, sum)
-#         return traverse(root, 0)
+#         return traverse(root,0)
