cb73a0f7375ba3003aba16138a05ed42b636bf67
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-12-16 18:40:19 +0500

    Time: 0 ms (100.00%) | Memory: 17.3 MB (18.11%) - LeetSync

--- 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.py
+++ 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.py
@@ -1,12 +1,45 @@
 class Solution:
     def getFinalState(self, nums: List[int], k: int, multiplier: int) -> List[int]:
-        for _ in range(k):
-            num = float("inf")
-            cur = 0
+
+        for j in range(k):
+            minnum = float("inf")
+            ind = 0
             for i in range(len(nums)):
-                if nums[i] < num:
-                    num = nums[i]
-                    cur = i
-            nums[cur] *= multiplier
+                if nums[i] < minnum:
+                    minnum = nums[i]
+                    ind = i
+            nums[ind] *= multiplier
         return nums
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # for _ in range(k):
+        #     num = float("inf")
+        #     cur = 0
+        #     for i in range(len(nums)):
+        #         if nums[i] < num:
+        #             num = nums[i]
+        #             cur = i
+        #     nums[cur] *= multiplier
+        # return nums
             
