00afd0b125ddaa88e3bd8936e75ea14271592528
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-12-16 18:52:34 +0500

    Time: 5 ms (43.87%) | Memory: 18.4 MB (10.22%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,37 +1,6 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        x = {}
-        for val in nums:
-            x[val] = val
-        print(x)
-        for i in range(len(x)):
-            if i not in nums:
+        hsh = {val:val for val in nums}
+        for i in range(len(nums) + 1):
+            if i not in hsh:
                 return i
-        return len(nums) 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        # nums.sort()
-        # if nums[0] != 0:
-        #     return 0
-        # x = 0
-        # for i in range(len(nums)):
-        #     if i not in nums:
-        #         x= i
-        # if x == 0:
-        #     return max(nums) + 1
-        # else:
-        #     return x
