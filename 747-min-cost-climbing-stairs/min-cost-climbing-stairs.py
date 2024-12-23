0b8a7ef27a3e2675a120ea2f09543375765c5396
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-12-23 11:04:52 +0500

    Time: 3 ms (73.21%) | Memory: 17.8 MB (21.86%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -1,21 +1,6 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        n = len(cost)
-        
-        # Base cases: cost to reach the first and second steps
-        if n == 1:
-            return cost[0]
-        elif n == 2:
-            return min(cost[0], cost[1])
-        
-        # dp[i] will store the minimum cost to reach step i
-        dp = [0] * n
-        dp[0] = cost[0]
-        dp[1] = cost[1]
-        
-        # Fill the dp array
-        for i in range(2, n):
-            dp[i] = cost[i] + min(dp[i-1], dp[i-2])
-        
-        # The answer will be the minimum cost to reach either of the last two steps
-        return min(dp[n-1], dp[n-2])
+        cost.append(0)
+        for i in range(len(cost) - 3, -1, -1):
+            cost[i] += min(cost[i+1], cost[i+2])
+        return min(cost[0], cost[1])
