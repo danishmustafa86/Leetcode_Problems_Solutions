93bbd87062bf0ee203da39cd2b6ab72e0d4f81ac
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2024-12-25 11:34:18 +0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (5.29%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -1,7 +1,6 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-            return nums[0]
+
         rob1, rob2 = 0, 0
         for i in range(len(nums) - 1):
             temp = max(rob1 + nums[i], rob2)
@@ -14,4 +13,4 @@ class Solution:
             rob1 = rob2
             rob2 = temp
 
-        return max(max1, rob2)
+        return max(nums[0],max1, rob2)
