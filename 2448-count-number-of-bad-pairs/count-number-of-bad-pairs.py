1216f31b4013b62b58b86a92382a5d956bcbb9b5
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2025-02-09 14:09:46 +0500

    Time: 89 ms (44.29%) | Memory: 38.9 MB (59.29%) - LeetSync

--- 2448-count-number-of-bad-pairs/count-number-of-bad-pairs.py
+++ 2448-count-number-of-bad-pairs/count-number-of-bad-pairs.py
@@ -1,22 +1,9 @@
-# class Solution:
-#     def countBadPairs(self, nums: List[int]) -> int:
-#         ans = 0
-#         for i in range(len(nums)):
-#             for j in range(i,len(nums)):
-#                 if j - i != nums[j] - nums[i]:
-#                     ans += 1
-#         return ans
-
-
+from collections import defaultdict
 class Solution:
     def countBadPairs(self, nums: List[int]) -> int:
-        count = defaultdict(int)
         ans = 0
-        
+        count = defaultdict(int)
         for i, num in enumerate(nums):
-            # The number of good pairs is the number of times (nums[i] - i) has been seen before
             ans += i - count[num - i]
-            # Update the count of (nums[i] - i)
             count[num - i] += 1
-        
         return ans
