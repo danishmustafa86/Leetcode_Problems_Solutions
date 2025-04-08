b04639cf9dffdaf446b3ef355be79c26db888580
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2025-04-09 00:35:48 +0500

    Time: 6 ms (37.60%) | Memory: 17.7 MB (93.12%) - LeetSync

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.py
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.py
@@ -1,26 +1,9 @@
 class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
-        from collections import Counter
-        freq = Counter(nums)
-
-        def is_distinct():
-            return all(v <= 1 for v in freq.values())
-
-        if is_distinct():
-            return 0
-
-        k = 0
-        while nums:
-            if len(nums) < 3:
-                return k + 1
-
-            for i in range(3):
-                freq[nums[i]] -= 1
-                if freq[nums[i]] == 0:
-                    del freq[nums[i]]
-            nums = nums[3:]
-            k += 1
-            if is_distinct():
-                return k
-
-        return k
+        count=0
+        while len(nums)>len(set(nums)):
+            nums=nums[3:]
+            count+=1
+        return count
+        
+        
