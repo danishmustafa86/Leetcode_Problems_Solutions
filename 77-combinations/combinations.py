c95ed670b7cc5b9b1abcb8a5c3527a230b34f88c
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2025-04-25 06:23:16 +0500

    Time: 162 ms (26.06%) | Memory: 59.7 MB (56.93%) - LeetSync

--- 77-combinations/combinations.py
+++ 77-combinations/combinations.py
@@ -1,57 +1,59 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
 
-        # arr  = []
-        # def fun(i,res):
-        #     if len(res) == k and res not in arr:
-        #         arr.append(res[:])
-        #         return
-        #     if i > n:
-        #         return
-        #     res.append(i)
-        #     fun(i+1, res)
-        #     res.pop()
-        #     fun(i+1,res)
-
-        # fun(1, [])
-        # return arr
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        resArr = []
-        cur = []
-        def dfs(i):
-            if i == n + 1:
-                resArr.append(cur.copy())
+        arr  = []
+        def fun(i,res):
+            if len(res) == k:
+                arr.append(res[:])
                 return
-            cur.append(i)
-            dfs( i+1 )
-            cur.pop()
-            dfs( i+1 )
+            if len(res) > k:
+                return
+            if i > n:
+                return
+            res.append(i)
+            fun(i+1, res)
+            res.pop()
+            fun(i+1,res)
+
+        fun(1, [])
+        return arr
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # resArr = []
+        # cur = []
+        # def dfs(i):
+        #     if i == n + 1:
+        #         resArr.append(cur.copy())
+        #         return
+        #     cur.append(i)
+        #     dfs( i+1 )
+        #     cur.pop()
+        #     dfs( i+1 )
         
-        dfs(1)
-        ans = []
-        for i in resArr:
-            if len(i) == k:
-                ans.append(i)
-        return ans
+        # dfs(1)
+        # ans = []
+        # for i in resArr:
+        #     if len(i) == k:
+        #         ans.append(i)
+        # return ans
