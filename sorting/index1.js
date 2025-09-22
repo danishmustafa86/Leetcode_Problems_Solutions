f84077bddd66dfb4839295336aa1041c9b47f5d1
Danish Mustafa <danishjajja86@gmail.com>
2025-09-22 08:45:30 -0700

    typescript leetcode solutions by hand

--- sorting/index1.js
+++ sorting/index1.js
@@ -1,4 +1,4 @@
-// You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and
+// You are given two intege arrays nums1 and nums2, sorted in non-decreasing order, and
 //  two integers m and n, representing the number of elements in nums1 and nums2 respectively.
 // Merge nums1 and nums2 into a single array sorted in non-decreasing order.
 // The final sorted array should not be returned by the function, but instead be stored 
