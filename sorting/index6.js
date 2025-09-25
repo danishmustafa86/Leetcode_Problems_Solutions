4af04b143dbf1f1389e6af7f1baaa4d7ef536b79
Danish Mustafa <danishjajja86@gmail.com>
2025-09-25 08:46:49 -0700

    typescript solutions of finding maximum numbers

--- sorting/index6.js
+++ sorting/index6.js
@@ -1,4 +1,4 @@
-// Given an integer array nums, return the third distinct maximum number in this array. If the third maximum does not exist, return the maximum number.
+// Given an integer array nums, return the third distinct maximum number in this aray. If the third maximum does not exist, return the maximum number.
 // Example 1:
 // Input: nums = [3,2,1]
 // Output: 1
