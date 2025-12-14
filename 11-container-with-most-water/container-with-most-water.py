a8e67d5dd143edf2cd9a6470df83cdfd45a61fe0
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2025-12-14 14:59:25 +0500

    Time: 102 ms (57.91%) | Memory: 28.4 MB (90.29%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -1,106 +1,12 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        max_area = 0
-        left  = 0
-        right = len(height) - 1
-        while left < right:
-            cur = min( height[left], height[right]) * (right - left)
-            max_area = max( max_area, cur)
-
-            if height[left] < height[right]:
-                left += 1
+        l, r = 0, len(height) - 1
+        maxarea = 0
+        while l < r:
+            curarea = min(height[l] , height[r]) * (r - l )
+            maxarea = max( curarea, maxarea)
+            if height[l] < height[r]:
+                l += 1
             else:
-                right -= 1
-        return max_area
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# class Solution:
-#     def maxArea(self, height: List[int]) -> int:
-#         max_area = 0
-#         left = 0
-#         right = len(height) - 1
-
-#         while left < right:
-#             current_area = min(height[left], height[right]) * (right - left)
-#             print(current_area)
-#             max_area = max(max_area, current_area)
-
-#             if height[left] < height[right]:
-#                 left += 1
-#             else:
-#                 right -= 1
-
-#         return max_area
-
-
-
-
-
-
-
-
+                r -= 1
+        return maxarea
