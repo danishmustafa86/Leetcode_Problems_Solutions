c70a905c4111576b0684a8918b7f1ec2bdfd2c61
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2025-12-21 15:22:27 +0500

    Time: 0 ms (100.00%) | Memory: 17.3 MB (99.20%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -1,14 +1,29 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
 
-
         for i in range(len(digits) - 1, -1, -1):
             if digits[i] < 9:
                 digits[i] += 1
                 return digits
             digits[i] = 0
+        return [1] + digits  
 
-        return [1] + digits
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         # n = len(digits)
         # for i in range(n-1,-1,-1):
