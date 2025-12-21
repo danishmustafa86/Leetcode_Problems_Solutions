93ee31e36caed2ddc09540f0f3ae8a771050978e
Danish Mustafa <135536662+danishmustafa86@users.noreply.github.com>
2025-12-21 18:02:33 +0500

    Time: 0 ms (100.00%) | Memory: 17.1 MB (99.68%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -1,33 +1,17 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
 
-        for i in range(len(digits) -1, -1, -1):
+        for i in range(len(digits) - 1, -1, -1):
             if digits[i] < 9:
-                digits[i] += 1
+                digits[i] = digits[i] + 1
                 return digits
             digits[i] = 0
         return [1] + digits
 
+# [9,9,9,9,9,9,9]
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-        # n = len(digits)
-        # for i in range(n-1,-1,-1):
-        #     if digits[i] <9:
-        #         digits[i] += 1
-        #         return digits
-        #     digits[i] = 0
-        # return [1] + digits
+# a) Considering all the values are 9.
+# b) Considering the last digit is not 9.
+# c) Considering the last digit is 9.
